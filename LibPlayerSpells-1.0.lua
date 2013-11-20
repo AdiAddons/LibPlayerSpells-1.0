d66991dfd0416092cd879bb77a331189651790e0
Adirelle <adirelle@gmail.com>
2013-11-20 19:08:29 +0100

    Core: changed the one-uppercase-letter upvalue names to real names.

--- LibPlayerSpells-1.0.lua
+++ LibPlayerSpells-1.0.lua
@@ -22,6 +22,8 @@ local MAJOR, MINOR = "LibPlayerSpells-1.0", 1
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 
+local bor, band = bit.bor, bit.band
+
 lib.constants = {
 	-- Classes
 	DEATHKNIGHT = 0x00000001,
@@ -53,11 +55,13 @@ lib.constants = {
 	RACIAL      = 0x00100000, -- Racial trait
 	TRADESKILL  = 0x00200000, -- Tradeskill bonus ability
 }
+local constants = lib.constants
 
 lib.masks = {
 	CLASS       = 0x000004FF,
 	TARGETING   = 0x000E0000,
 }
+local masks = lib.masks
 
 lib.spells = lib.spells or {
 	all         = {},
@@ -76,24 +80,20 @@ lib.spells = lib.spells or {
 	WARLOCK     = {},
 	WARRIOR     = {},
 }
+local spells = lib.spells
 
 lib.versions = lib.versions or {}
+local versions = lib.versions
 
 lib.aliases = lib.aliases or {}
+local aliases = lib.aliases
 
 -- Useful upvalues
-local C = lib.constants
-local M = lib.masks
-local S = lib.spells
-local V = lib.versions
-local A = lib.aliases
-local bor = bit.bor
-local band = bit.band
 
 local function ParseFilter(filter)
 	local flags = 0
 	for word in filter:gmatch("%a+") do
-		local value = C[word] or M[word]
+		local value = constants[word] or masks[word]
 		if not value then
 			error(format("%s: invalid filter: %q",  MAJOR, tostring(filter)), 5)
 		end
@@ -115,16 +115,16 @@ lib.__filters = setmetatable(
 		end,
 	}
 )
-local F = lib.__filters
+local filters = lib.__filters
 
 --- Return version information about a category
 -- @param category (string) The category.
 -- @return (number) The version information.
 function lib:GetVersionInfo(category)
-	if not S[category] then
+	if not spells[category] then
 		error(format("%s: invalid category: %q", MAJOR, tostring(category)), 2)
 	end
-	return V[category] or 0
+	return versions[category] or 0
 end
 
 local TRUE = function() return true end
@@ -132,9 +132,9 @@ local TRUE = function() return true end
 -- Parse filtering parameters
 function lib:GetFlagTester(anyOf, include, exclude)
 	if not anyOf and not restrict and not exclude then return TRUE end
-	local anyOfMask = anyOf and F[anyOf] or 0
-	local includeMask = include and F[include] or 0
-	local excludeMask = exclude and F[exclude] or 0
+	local anyOfMask = anyOf and filters[anyOf] or 0
+	local includeMask = include and filters[include] or 0
+	local excludeMask = exclude and filters[exclude] or 0
 	local mask = bor(includeMask, excludeMask)
 	local expected = bit.bxor(mask, excludeMask)
 	if mask == 0 then
@@ -158,16 +158,16 @@ end
 -- @return (function) The tester function.
 function lib:GetSpellTester(anyOf, include, exclude)
 	local tester = lib:GetFlagTester(anyOf, include, exclude)
-	return function(spellId) return tester(S.all[spellId or false] or 0) end
+	return function(spellId) return tester(spells.all[spellId or false] or 0) end
 end
 
 -- Filtering iterator
 local function filterIterator(tester, index)
 	local flags
 	repeat
-		spellId, flags = next(S.all, spellId)
+		spellId, flags = next(spells.all, spellId)
 		if spellId and tester(flags) then
-			return spellId, flags, A[spellId]
+			return spellId, flags, aliases[spellId]
 		end
 	until not spellId
 end
@@ -182,7 +182,7 @@ end
 -- The iterator returns the category name and the spells in that category.
 -- @return An iterator suitable for .. in .. do loops.
 function lib:IterateCategories()
-	return pairs(S)
+	return pairs(spells)
 end
 
 --- Return the flags of a spell
@@ -190,24 +190,24 @@ end
 -- @return (number) The spell flags or nil if it is unknown.
 -- @return (number) The spellbook spell identifier.
 function lib:GetSpellInfo(spellId)
-	local flags = spellId and S.all[spellId]
+	local flags = spellId and spells.all[spellId]
 	if flags then
-		return flags, A[spellId]
+		return flags, aliases[spellId]
 	end
 end
 
 -- Used to register a category of spells
-function lib:__RegisterSpells(category, interface, minor, spells, aliases)
-	if not S[category] then
+function lib:__RegisterSpells(category, interface, minor, newSpells, newAliases)
+	if not spells[category] then
 		error(format("%s: invalid category: %q", MAJOR, tostring(category)), 2)
 	end
 	local version = tonumber(interface) * 100 + minor
 
-	if (V[category] or 0) >= version then return end
-	V[category] = version
+	if (versions[category] or 0) >= version then return end
+	versions[category] = version
 
 	-- Wipe existing spells for that class
-	local all, db = S.all, S[category]
+	local all, db = spells.all, spells[category]
 	for id in pairs(db) do
 		db[id] = nil
 		all[id] = nil
@@ -215,17 +215,17 @@ function lib:__RegisterSpells(category, interface, minor, spells, aliases)
 	end
 
 	-- Rebuild the flags
-	local catFlags = C[category] or 0
-	for key, value in pairs(spells) do
+	local catFlags = constants[category] or 0
+	for key, value in pairs(newSpells) do
 		if type(key) == "string" and type(value) == "table" then
 			-- key is a filter, value a list of spell ids
-			local flags = F[key]
+			local flags = filters[key]
 			for i, spellId in ipairs(value) do
 				db[spellId] = bor(db[spellId] or 0, flags, catFlags)
 			end
 		elseif type(key) == "number" then
 			-- key is a spell id, value a flag
-			db[key] = bor(db[key] or 0, F[value], catFlags)
+			db[key] = bor(db[key] or 0, filters[value], catFlags)
 		else
 			error(format("%s: invalid spell data: (%q, %q)", MAJOR, tostring(key), tostring(value)), 2)
 		end
@@ -237,7 +237,7 @@ function lib:__RegisterSpells(category, interface, minor, spells, aliases)
 			error(format("%s: unknown spell #%d", MAJOR, spellId), 2)
 		end
 		all[spellId] = db[spellId]
-		A[spellId] = aliases and aliases[spellId] or spellId
+		aliases[spellId] = newAliases and newAliases[spellId] or spellId
 	end
 
 end
