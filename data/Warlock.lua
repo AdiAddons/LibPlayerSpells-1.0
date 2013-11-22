e2416eb90787f6bf7c40708b74c4c4bf12fa0f33
Adirelle <adirelle@gmail.com>
2013-11-22 22:03:16 +0100

    Warlock: first version.

--- data/Warlock.lua
+++ data/Warlock.lua
@@ -20,3 +20,100 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
+lib:__RegisterSpells("WARLOCK", "50400", 1, {
+	COOLDOWN = {
+		   755, -- Health Funnel (glyphed)
+		  1122, -- Summon Infernal
+		 18540, -- Summon Doomguard
+		 29858, -- Soulshatter
+		105174, -- Hand of Gul'dan
+		108482, -- Unbound Will
+		108501, -- Grimoire of Service
+		114175, -- Demonic Slash (Dark Apotheosis)
+		120451, -- Flames of Xoroth
+		132411, -- Singe Magic (sacrified Imp)
+		AURA = {
+			PERSONAL = {
+				 48018, -- Demonic Circle: Summon
+				 48020, -- Demonic Circle: Teleport
+				104025, -- Immolation Aura (Metamorphosis/Dark Apotheosis)
+				108503, -- Grimoire of Sacrifice (talent)
+				116198, -- Aura of Enfeeblement (Metamorphosis/Dark Apotheosis)
+				SURVIVAL = {
+					  6229, -- Twilight Ward
+					 91713, -- Nether Ward (talent)
+					104773, -- Unending Resolve
+					108359, -- Dark Regeneration
+					108416, -- Sacrificial Pact
+					110913, -- Dark Bargain
+					119839, -- Fury Ward (Dark Apotheosis)
+					132413, -- Shadow Bulwark (Grimoire of Sacrifice)
+				},
+				BURST = {
+					 80240, -- Havoc
+					108508, -- Mannoroth's Fury (talent)
+					113858, -- Dark Soul: Instability
+					113860, -- Dark Soul: Misery
+					113861, -- Dark Soul: Knowledge
+				},
+			},
+			HEPLFUL = {
+				[20707] = "UNIQUE_AURA", -- Soulstone
+			},
+			HARMFUL = {
+				  5484, -- Howl of Terror
+				  6789, -- Mortal Coil
+				 17962, -- Conflagrate
+				 30283, -- Shadowfury
+				 47897, -- Demonic Breath
+				111397, -- Blood Horror
+				124917, -- Metamorphosis: Chaos Wave
+			},
+		},
+	},
+	AURA = {
+		PERSONAL = {
+			108559, -- Demonic Rebirth
+			108563, -- Backlash
+			114635, -- Ember Tap
+			117828, -- Backdraft (buff)
+			122351, -- Molten Core
+		},
+		HELPFUL = {
+			[5697] = "UNIQUE_AURA", -- Unending Breath
+		},
+		PET = {
+			   755, -- Health Funnel
+			  1098, -- Enslave Demon
+		},
+		HARMFUL = {
+			   172, -- Corruption
+			   348, -- Immolate
+			   603, -- Metamorphosis: Doom
+			   980, -- Agony
+			 27243, -- Seed of Corruption
+			 30108, -- Unstable Affliction
+			 47960, -- Shadowflame
+			 48181, -- Haunt
+			108686, -- Immolate (Fire and Brimstone)
+		},
+	},
+	RAIDBUFF = {
+		[109773] = "SPL_POWER STAMINA", -- Dark Intent
+	},
+}, {
+	-- Map aura to provider
+	[ 47960] = 105174, -- Shadowflame <= Hand of Gul'dan
+	[117828] = 117896, -- Backdraft
+}, {
+	-- Map aura to modified spell(s)
+	[108563] = { -- Backlash =>
+		 29722, -- Incinerate
+		114654, -- Incinerate (Fire and Brimstone)
+	},
+	[117828] = { -- Backdraft =>
+		 29722, -- Incinerate
+		114654, -- Incinerate (Fire and Brimstone)
+	},
+	[122351] = 6353, -- Molten Core => Soul Fire
+})
