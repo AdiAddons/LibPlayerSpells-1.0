256cfb0cacfee4390ba1717cb4edebd3a51531b3
Guillaume Perréal <guillaume.perreal@irstea.fr>
2013-11-26 09:30:33 +0100

    Priest: added a bunch of aura and procs.

--- data/Priest.lua
+++ data/Priest.lua
@@ -20,11 +20,12 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("PRIEST", "50400", 3, {
+lib:__RegisterSpells("PRIEST", "50400", 4, {
 	COOLDOWN = {
 		   527, -- Purify
 		 19236, -- Desperate Prayer
 		 32375, -- Mass Dispel
+		 47540, -- Penance
 		 62618, -- Power Word: Barrier
 		 64843, -- Divine Hymn
 		 73325, -- Leap of Faith
@@ -51,16 +52,40 @@ lib:__RegisterSpells("PRIEST", "50400", 3, {
 				},
 			},
 			HELPFUL = {
+				   17, -- Power Word: Shield
 				33076, -- Prayer of Mending
 				SURVIVAL = {
 					33206, -- Pain Suppression
 					47788, -- Guardian Spirit
 				},
+				UNIQUE_AURA = {
+					6346, -- Fear Ward
+				},
 			},
 			HARMFUL = {
-				8122, -- Psychic Scream
+				  8122, -- Psychic Scream
+				 14914, -- Holy Fire
 				108920, -- Void Tendrils
+				129250, -- Power Word: Solace
 			},
+		},
+	},
+	AURA = {
+		PERSONAL = {
+			   588, -- Inner Fire
+			 63735, -- Serendipity
+			 73413, -- Inner Will
+			 81662, -- Evangelism
+			112833, -- Spectral Guise
+			114255, -- Surge of Light
+		},
+		HELPFUL = {
+			   139, -- Renew
+			[ 1706] = "UNIQUE_AURA", -- Levitate
+		HARMFUL = {
+			   589, -- Shadow Word: Pain
+			  2944, -- Devouring Plague
+			 34914, -- Vampiric Touch
 		},
 	},
 	RAIDBUFF = {
@@ -69,6 +94,14 @@ lib:__RegisterSpells("PRIEST", "50400", 3, {
 	},
 }, {
 	-- Map aura to provider
+	[ 63735] =  63733, -- Serendipity
+	[114255] = 109186, -- Surge of Light <= From Darkness, Comes Light
 }, {
 	-- Map aura to modified spell(s)
+	[ 63735] = { -- Serendipity =>
+		 596, -- Prayer of Healing
+		2060, -- Greater Heal
+	}
+	[ 81662] = 81700, -- Evangelism => Archangel
+	[114255] =  2061, -- Surge of Light => Flash Heal
 })
