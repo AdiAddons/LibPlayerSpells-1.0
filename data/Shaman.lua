daa5789d3f04f7a7a4dcbce2f8562327d266a6b9
Adirelle <adirelle@gmail.com>
2014-01-03 23:21:10 +0100

    Shaman: added several spells for elemental spec.
    Code provided by ckaotik, see #1.

--- data/Shaman.lua
+++ data/Shaman.lua
@@ -20,8 +20,93 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("SHAMAN", "50400", 2, {
-	[57994] = "COOLDOWN INTERRUPT", -- Wind Shear
+lib:__RegisterSpells("SHAMAN", "50400", 3, {
+	COOLDOWN = {
+		[16190] = 'MANA_REGEN',          -- Mana Tide Totem
+		[51490] = 'PERSONAL MANA_REGEN', -- Thunderstorm
+		  2484, -- Earthbind Totem
+		  5394, -- Healing Stream Totem
+		  8143, -- Tremor Totem
+		  8177, -- Grounding Totem
+		 51485, -- Earthgrab Totem (talent)
+		108269, -- Capacitator Totem
+		 16188, -- Ancestral Swiftness (talent)
+		 20608, -- Reincarnation
+		 51505, -- Lava Burst
+		 61882, -- Earthquake
+		 73680, -- Unleash Elements
+		108281, -- Ancestral Guidance (talent)
+		108285, -- Call of the Elements (talent)
+		117012, -- Unleashed Fury (talent)
+		117014, -- Elemental Blast (talent)
+		INTERRUPT = {
+			57994, -- Wind Shear
+			113287, -- Solar Beam (Symbiosis)
+		},
+		AURA = {
+			[ 61295] = 'HELPFUL', -- Riptide
+			[120668] = 'BURST', -- Stormlash Totem
+			PERSONAL = {
+				SURVIVAL = {
+					  2062, -- Earth Elemental Totem
+					 30823, -- Shamanistic Rage
+					 98008, -- Spirit Link Totem
+					108270, -- Stone Bulwark Totem (talent)
+					108271, -- Astral Shift (talent)
+					108280, -- Healing Tide Totem
+				},
+				BURST = {
+					  2894, -- Fire Elemental Totem
+					 16166, -- Elemental Mastery (talent)
+					 51533, -- Feral Spirit
+					114049, -- Ascendance
+				},
+				79206, -- Spiritwalker's Grace
+				73920, -- Healing Rain (indicator)
+			},
+		},
+	},
+	AURA = {
+		PERSONAL = {
+			IMPORTANT = {
+				16246, -- Clearcasting (buff)
+				53390, -- Tidal Waves
+			},
+			UNIQUE_AURA = {
+				  324, -- Lightning Shield
+				52127, -- Water Shield
+			},
+			  8024, -- Flametongue Weapon
+			  8033, -- Frostbrand Weapon
+			  8017, -- Rockbiter Weapon
+			  8232, -- Windfury Weapon
+			 51730, -- Earthliving Weapon
+			 73681, -- Unleash Wind
+			 73683, -- Unleash Flame
+			 73685, -- Unleash Life
+			118472, -- Unleashed Fury (Windfury)
+			118473, -- Unleashed Fury (Earthliving)
+			118474, -- Unleashed Fury (Rockbiter)
+			118475, -- Unleashed Fury (Frostbrand)
+			114050, -- Ascendance (Elemental)
+			114051, -- Ascendance (Enhancement)
+			114052, -- Ascendance (Restoration)
+			118522, -- Elemental Blast (talent)
+		},
+		HELPFUL = {
+			[  974] = 'UNIQUE_AURA', -- Earth Shield
+			 51945, -- Earthliving (proc)
+			105284, -- Ancestral Vigor (applied by Riptide)
+		},
+		HARMFUL = {
+			  8034, -- Frostbrand (proc)
+			  8050, -- Flame Shock
+			  8056, -- Frost Shock
+			 73682, -- Unleash Frost
+			 73684, -- Unleash Earth (taunt)
+			118470, -- Unleashed Fury (Flametongue)
+		},
+	},
 	RAIDBUFF = {
 		[  2825] = 'BURST_HASTE', -- Bloodlust (Horde shaman)
 		[ 30809] = 'ATK_SPEED',   -- Unleashed Rage
@@ -38,6 +123,26 @@ lib:__RegisterSpells("SHAMAN", "50400", 2, {
 	},
 }, {
 	-- Map aura to provider
+	[ 51945] =  51730, -- Earthliving
+	[ 53390] =  51564, -- Tidal Waves
+	[ 73683] =  73680, -- Unleash Elements (Flametongue)
+	[ 73681] =  73680, -- Unleash Elements (Windfury)
+	[ 73685] =  73680, -- Unleash Elements (Earthliving)
+	[ 73684] =  73680, -- Unleash Elements (Rockbiter)
+	[ 73682] =  73680, -- Unleash Elements (Frostbrand)
+	[118470] = 117012, -- Unleashed Fury (Flametongue)
+	[118472] = 117012, -- Unleashed Fury (Windfury)
+	[118473] = 117012, -- Unleashed Fury (Earthliving)
+	[118474] = 117012, -- Unleashed Fury (Rockbiter)
+	[118475] = 117012, -- Unleashed Fury (Frostbrand)
+	[118522] = 117014, -- Elemental Blast
+	[114050] = 114049, -- Ascendance (Elemental)
+	[114051] = 114049, -- Ascendance (Enhancement)
+	[114052] = 114049, -- Ascendance (Restoration)
 }, {
 	-- Map aura to modified spell(s)
+	[114050] = { 114074, 51505 }, -- Ascendance (Elemental): Chain Lightning, Lava Burst
+	[114051] = 17364, -- Ascendance (Enhancement): Stormstrike
+	[114052] = { 61295, 73920, 1064, 8004, 77472, 331 }, -- Ascendance (Restoration): Riptide, Healing Rain, Chain Heal, Healing Surge, Greater Healing Wave, Healing Wave
+	[ 53390] = { 8004, 77472, 331 }, -- Tidal Waves
 })
