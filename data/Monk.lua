63c683ddf130bf5ba62a23db40c37e9ffef5cc88
Rainrider <rainrider.wow@gmail.com>
2014-08-12 13:06:04 +0200

    monk: add windwalker spells

--- data/Monk.lua
+++ data/Monk.lua
@@ -34,6 +34,9 @@ lib:__RegisterSpells("MONK", "60000", 1, {
 				-- 121253, -- Keg Smash -- XXX: seems to be the same as dizzying haze -- FIXME: ABA freezes the client on aura refresh
 				118585, -- Leer of the Ox
 				115078, -- Paralysis
+				120086, -- Fists of Fury -- XXX: added through DRData (stuns)
+				123586, -- Flying Serpent Kick -- TODO: add to snares in Common in ABA
+				-- 130320, -- Rising Sun Kick -- FIXME: ABA freezes the client on aura refresh
 			},
 			PERSONAL = {
 				SURVIVAL = {
@@ -42,7 +45,9 @@ lib:__RegisterSpells("MONK", "60000", 1, {
 					115295, -- Guard
 					137562, -- Nimble Brew
 					115176, -- Zen Meditation -- FIXME: buff currently unvisible. move to cooldowns if not resolved by blizz
+					125174, -- Touch of Karma
 				},
+				[115288] = "POWER_REGEN", -- Energizing Brew
 			},
 		},
 	},
@@ -51,6 +56,10 @@ lib:__RegisterSpells("MONK", "60000", 1, {
 			-- 123725, -- Breath of Fire -- FIXME: ABA freezes the client on aura refresh
 			-- 117952, -- Crackling Jade Lightning -- FIXME: ABA freezes the client on aura refresh
 			-- 116330, -- Dizzying Haze -- FIXME: ABA freezes the client on aura refresh
+			-- 128531, -- Blackout Kick (Stance of the Fierce Tiger) -- FIXME: ABA freezes the client on aura refresh
+			-- 116095, -- Disable (Slow) -- TODO: add to snares in Common in ABA -- FIXME: ABA freezes the client on aura refresh
+			-- 116706, -- Disable (Root) -- XXX: added through DRData
+			121125, -- Death Note
 		},
 		HELPFUL = {
 
@@ -59,6 +68,10 @@ lib:__RegisterSpells("MONK", "60000", 1, {
 			-- 115307, -- Shuffle -- FIXME: ABA freezes the client on aura refresh
 			101546, -- Spinning Crane Kick
 			-- 125359, -- Tiger Power -- FIXME: ABA freezes the client on aura refresh
+			-- 118864, -- Combo Breaker: Tiger Palm -- XXX: covered by the default ui
+			-- 116768, -- Combo Breaker: Blackout Kick -- XXX: covered by the default ui
+			137639, -- Storm, Earth and Fire
+			-- 116740, -- Tigereye Brew -- FIXME: ABA freezes the client on aura refresh
 		},
 	},
 	RAIDBUFF = {
@@ -77,8 +90,21 @@ lib:__RegisterSpells("MONK", "60000", 1, {
 	[120954] = 115203, -- Fortifying Brew
 	[118585] = 115543, -- Leer of the Ox
 	-- [125359] = 115069, -- Tiger Power <-- Stance of the Sturdy Ox -- FIXME: ABA freezes the client on aura refresh
+	-- [125359] = 103985, -- Tiger Power <-- Stance of the Fierce Tiger -- FIXME: ABA freezes the client on aura refresh
+	-- [128531] = 103985, -- Blackout Kick <-- Stance of the Fierce Tiger -- FIXME: ABA freezes the client on aura refresh
+	-- [116706] = 116095, -- Disable (Root) -- XXX: added through DRData (roots)
+	-- [118864] = 103985, -- Combo Breaker: Tiger Palm <-- Stance of the Fierce Tiger -- XXX: covered by the default ui
+	-- [116768] = 103985, -- Combo Breaker: Blackout Kick <-- Stance of the Fierce Tiger -- XXX: covered by the default ui
+	[120086] = 113656, -- Fists of Fury -- XXX: added through DRData (stuns)
+	[123586] = 101545, -- Flying Serpent Kick
+	-- [130320] = 107428, -- Rising Sun Kick -- FIXME: ABA freezes the client on aura refresh
+	[121125] = 115080, -- Death Note <-- Touch of Death
+	[125174] = 122470, -- Touch of Karma
 }, {
 	-- Map aura to modified spell(s)
 	-- [115307] = 100784, -- Shuffle --> Blackout Kick -- FIXME: ABA freezes the client on aura refresh
 	-- [125359] = 100787, -- Tiger Power --> Tiger Palm -- FIXME: ABA freezes the client on aura refresh
+	-- [128531] = 100784, -- Blackout Kick (Stance of the Fierce Tiger) -- FIXME: ABA freezes the client on aura refresh
+	-- [118864] = 100787, -- Combo Breaker: Tiger Palm --> Tiger Palm -- XXX: covered by the default ui
+	-- [116768] = 100784, -- Combo Breaker: Blackout Kick --> Blackout Kick -- XXX: covered by the default ui
 })
