84022d873f2fcc8ac5b92df309b77404f8f7d235
arcadepro <arcadepro@users.noreply.github.com>
2014-08-15 22:02:36 +0100

    Update Warrior.lua

--- data/Warrior.lua
+++ data/Warrior.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("WARRIOR", "50400", 7, {
+lib:__RegisterSpells("WARRIOR", "50400", 6, {
         POWER_REGEN = {
                 2457,   -- Battle Stance
                 2458,   -- Berserker Stance
