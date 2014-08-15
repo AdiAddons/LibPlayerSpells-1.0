8334382ed0c8a8a62c8eb3cf2051e8090b3fc3e1
Adirelle <adirelle@gmail.com>
2014-08-15 14:25:29 +0200

    Hunter: enabled hunter (de)buffs again.

--- data/Hunter.lua
+++ data/Hunter.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("HUNTER", "60000", 1, {
+lib:__RegisterSpells("HUNTER", "60000", 2, {
 	COOLDOWN = {
 		[147362] = "INTERRUPT", -- Counter Shot
 		    781, -- Disengage
@@ -38,11 +38,11 @@ lib:__RegisterSpells("HUNTER", "60000", 1, {
 			HARMFUL = {
 				 1462, -- Beast Lore
 				 3674, -- Black Arrow
-				--  5116, -- Concussive Shot -- XXX: added though Common rules in ABA
+				--  5116, -- Concussive Shot -- XXX: added through Common rules in ABA
 				-- 20736, -- Distracting Shot -- XXX: added through DRData (taunt)
 				 53301, -- Explosive Shot
-				-- 13812, -- Explosive Trap -- FIXME: ABA freezes the client on aura refresh
-				--  3355, -- Freezing Trap -- FIXME: ABA freezes the client on aura refresh
+				 13812, -- Explosive Trap
+				  3355, -- Freezing Trap
 				135299, -- Ice Trap
 				 25809, -- Crippling Poison
 				-- 136634, -- Narrow Escape (talent) -- XXX: added through DRData (root)
@@ -67,14 +67,14 @@ lib:__RegisterSpells("HUNTER", "60000", 1, {
 				[ 51755] = "SURVIVAL", -- Camouflage
 				[157760] = "SURVIVAL", -- Camouflage (with Improved Camouflage)
 				[148467] = "SURVIVAL", -- Deterrence
-				  -- 56453, -- Lock and Load -- FIXME: ABA freezes the client
+				  56453, -- Lock and Load
 				[  5384] = "SURVIVAL", -- Feign Death
 				  35079, -- Misdirection
 				[ 19574] = "BURST", -- Bestial Wrath
 				[ 82692] = "BURST", -- Focus Fire
-				  -- 53257, -- Cobra Strikes -- FIXME: ABA freezes the client
+				  53257, -- Cobra Strikes
 				[  3045] = "BURST", -- Rapid Fire
-				-- [ 82921] = "BURST", -- Bombardment -- FIXME: ABA freezes the client on aura refresh
+				[ 82921] = "BURST", -- Bombardment
 				 118922, -- Posthaste (talent)
 				[ 82726] = "POWER_REGEN", -- Fervor (talent)
 			},
@@ -82,17 +82,17 @@ lib:__RegisterSpells("HUNTER", "60000", 1, {
 	},
 	AURA = {
 		HARMFUL = {
-			-- 118253, -- Serpent Sting -- FIXME: ABA freezes the client
+			118253, -- Serpent Sting
 			162534, -- Poisoned Ammo (Exotic Munitions talent)
 			162546, -- Frozen Ammo (Exotic Munitions talent) -- TODO: add to snares in Common in ABA
 		},
 		PET = {
-			136, -- Mend Pet -- FIXME: ABA freezes the client on aura refresh
+			136, -- Mend Pet
 		},
 		PERSONAL = {
 			  6197, -- Eagle Eye
 			  1515, -- Tame Beast
-			 -- 34720, -- Thrill of the Hunt (talent) -- FIXME: ABA freezes the client
+			 34720, -- Thrill of the Hunt (talent)
 			162537, -- Poisoned Ammo (Exotic Munitions talent)
 			162536, -- Incendiary Ammo (Exotic Munitions talent)
 			162539, -- Frozen Ammo (Exotic Munitions talent)
@@ -137,18 +137,18 @@ lib:__RegisterSpells("HUNTER", "60000", 1, {
 	},
 }, {
 	-- Map aura to provider
-	-- [118253] = 87935, -- Serpent Sting -- FIXME: ABA freezes the client
+	[118253] =   3044, -- Serpent Sting <= Arcane Shot
 	[ 51755] =  51753, -- Camouflage
 	[157760] = 157753, -- Camouflage <-- Improved Camouflage
-	-- [ 56453] = 53301, -- Lock and Load <- Explosive Shot -- FIXME: ABA freezes the client
-	-- [ 13812] = { -- FIXME: ABA freezes the client on aura refresh
-	-- 	13813, -- Explosive Trap
-	-- 	82939, -- Explosive Trap (Trap Launcher)
-	-- },
-	-- [  3355] = { -- FIXME: ABA freezes the client on aura refresh
-	-- 	 1499, -- Freezing Trap
-	-- 	60192, -- Freezing Trap (Trap Launcher)
-	-- },
+	[ 56453] = 53301, -- Lock and Load <- Explosive Shot
+	[ 13812] = {
+		13813, -- Explosive Trap
+		82939, -- Explosive Trap (Trap Launcher)
+	},
+	[  3355] = {
+		 1499, -- Freezing Trap
+		60192, -- Freezing Trap (Trap Launcher)
+	},
 	[135299] = {
 		13809, -- Ice Trap
 		82941, -- Ice Trap (Trap Launcher)
@@ -159,32 +159,32 @@ lib:__RegisterSpells("HUNTER", "60000", 1, {
 	},
 	[ 54216] = 53271, -- Master's Call
 	[ 35079] = 34477, -- Misdirection
-	-- [ 53257] = 53260, -- Cobra Strikes -- FIXME: ABA freezes the client
-	-- [ 82921] = 35110, -- Bombardment -- FIXME: ABA freezes the client on aura refresh
+	[ 53257] = 53260, -- Cobra Strikes
+	[ 82921] = 35110, -- Bombardment
 	[118922] = 109215, -- Posthaste (talent)
 	-- [136634] = 109298, -- Narrow Escape (talent) -- XXX: added through DRData (root)
 	-- [117526] = 109248, -- Binding Shot (talent) -- XXX: added through DRData (stun)
 	-- [ 24394] =  19577, -- Intimidation (talent) -- XXX: added through DRData (stun)
-	-- [ 34720] = 109306, -- Thrill of the Hunt (talent) -- FIXME: ABA freezes the client
+	[ 34720] = 109306, -- Thrill of the Hunt (talent)
 	[120761] = 117050, -- Glaive Toss (talent) -- TODO: 2 ids same effect; add to snares in Common rules in ABA
 	[121414] = 117050, -- Glaive Toss (talent) -- TODO: 2 ids same effect; add to snares in Common rules in ABA
 	[162546] = 162534, -- Frozen Ammo <-- Exotic Munitions (talent) -- TODO: add to snares in Common in ABA -- FIXME: LibSpellbook says it's unknown when it isn't
 }, {
 	-- Map aura to modified spell(s)
-	-- [118253] = { -- FIXME: ABA freezes the client
-	-- 	3044, -- Arcane Shot
-	-- 	2643, -- Multi-Shot
-	-- },
+	[118253] = {
+		3044, -- Arcane Shot
+		2643, -- Multi-Shot
+	},
 	[157760] = 157754, -- Camouflage (with Improved Camouflage)
-	-- [ 53257] = 3044, -- Cobra Strikes --> Arcane Shot -- FIXME: ABA freezes the client
-	-- [ 82921] = 2643, -- Bombardment --> Multi-Shot -- FIXME: ABA freezes the client on aura refresh
+	[ 53257] = 3044, -- Cobra Strikes --> Arcane Shot
+	[ 82921] = 2643, -- Bombardment --> Multi-Shot
 	[118922] = 781, -- Posthaste (talent) --> Disengage
 	-- [136634] = 781, -- Narrow Escape (talent) --> Disengage -- XXX: added through DRData (root)
-	-- [ 34720] = { -- Thrill of the Hunt (talent) -- FIXME: ABA freezes the client
-	-- 	19434, -- Aimed Shot
-	-- 	 2643, -- Multi-Shot
-	-- 	 3044, -- Arcane Shot
-	-- },
+	[ 34720] = { -- Thrill of the Hunt (talent)
+		19434, -- Aimed Shot
+		 2643, -- Multi-Shot
+		 3044, -- Arcane Shot
+	},
 	[162534] = 75, -- Poisoned Ammo --> Auto Shot
 	[162546] = 75, -- Frozen Ammo --> Auto Shot -- TODO: add to snares in Common in ABA
 })
