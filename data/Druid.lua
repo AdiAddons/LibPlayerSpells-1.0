bd9bf6b862c4df96c2bddba52a490107a5359f91
Adirelle <adirelle@gmail.com>
2014-08-31 20:19:16 +0200

    Druid: revision bump.

--- data/Druid.lua
+++ data/Druid.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("DRUID", "50400", 6, {
+lib:__RegisterSpells("DRUID", "50400", 7, {
 	COOLDOWN = {
 		   740, -- Tranquility
 		 18562, -- Swiftmend
