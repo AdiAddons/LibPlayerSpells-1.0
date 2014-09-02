246ace861756640efb425fd866cfd53628316d7f
arcadepro <orochi_pocket@hotmail.com>
2014-09-02 21:44:27 +0100

    Fixed HotW again.

--- data/Druid.lua
+++ data/Druid.lua
@@ -140,10 +140,10 @@ lib:__RegisterSpells("DRUID", "50400", 7, {
 	[102543] = 106731, -- Incarnation: King of the Jungle
 	[102558] = 106731, -- Incarnation: Son of Ursoc
 	[102560] = 106731, -- Incarnation: Chosen of Elune
-	[108291] = 108288, -- Heart of the Wild (Balance)
-	[108292] = 108288, -- Heart of the Wild (Feral)
-	[108293] = 108288, -- Heart of the Wild (Guardian)
-	[108294] = 108288, -- Heart of the Wild (Resto)
+	-- [108291] = 108288, -- Heart of the Wild (Balance) -- Adding these lines breaks the HotW aura display.
+	-- [108292] = 108288, -- Heart of the Wild (Feral)
+	-- [108293] = 108288, -- Heart of the Wild (Guardian)
+	-- [108294] = 108288, -- Heart of the Wild (Resto)
 	[114108] = 114107, -- Soul of the Forest (restoration)
 	[145151] = 108373, -- Dream of Cenarius (balance)
 	[145152] = 108373, -- Dream of Cenarius (feral)
