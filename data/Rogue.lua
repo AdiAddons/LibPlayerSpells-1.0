94ec0d2b3a874a34f3d02bc07ca4470988ed9035
Adirelle <adirelle@gmail.com>
2014-09-20 19:28:27 +0200

    Bump data revisions.

--- data/Rogue.lua
+++ data/Rogue.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("ROGUE", "50400", 4, {
+lib:__RegisterSpells("ROGUE", "50400", 5, {
 	COOLDOWN = {
 		INTERRUPT = {
 			1766, -- Kick
--- data/Warlock.lua
+++ data/Warlock.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("WARLOCK", "50400", 6, {
+lib:__RegisterSpells("WARLOCK", "50400", 7, {
 	COOLDOWN = {
 		  1122, -- Summon Infernal
 		 18540, -- Summon Doomguard
