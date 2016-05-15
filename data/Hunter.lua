0953d205647c43225ddab408ec90742a39e4a870
Rainrider <Rainrider@users.noreply.github.com>
2016-05-15 05:32:41 +0200

    hunter: add beastmaster spells
    TODO talents, artifact

--- data/Hunter.lua
+++ data/Hunter.lua
@@ -20,4 +20,45 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("HUNTER", "70000", 1, {})
+lib:__RegisterSpells("HUNTER", "70000", 1, {
+    COOLDOWN = {
+            781, -- Disengage
+           1543, -- Flare
+          34026, -- Kill Command
+         109304, -- Exhilaration
+        [147362] = "INTERRUPT", -- Counter Shot
+        AURA = {
+            HARMFUL = {
+                5116, -- Concussive Shot
+            },
+            PERSONAL = {
+                   5384, -- Feign Death
+                  35079, -- Misdirection
+                 186257, -- Aspect of the Cheetah (90%)
+                 186258, -- Aspect of the Cheetah (30%)
+                 193530, -- Aspect of the Wild
+                 120694, -- Dire Beast
+                [ 19574] = "BURST", -- Bestial Wrath
+                [186265] = "SURVIVAL", -- Aspect of the Turtle
+
+            },
+            PET = {
+                   136, -- Mend Pet
+                118455, -- Beast Cleave
+            },
+        },
+    },
+    AURA = {
+        [1462] = "HARMFUL", -- Beast Lore (NOTE: HELPFUL on player controlled pets, HARMFUL else)
+        [1515] = "HARMFUL", -- Tame Beast
+        [6197] = "PERSONAL", -- Eagle Eye
+    },
+}, {
+    -- map aura to provider(s)
+    [ 35079] = 34477, -- Misdirection
+    [118455] = 2643, -- Beast Cleave
+    [120694] = 120679, -- Dire Beast
+    [186258] = 186257, -- Aspect of the Cheetah
+}, {
+    -- map aura to modified spell(s)
+})
