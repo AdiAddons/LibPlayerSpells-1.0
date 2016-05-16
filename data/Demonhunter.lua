1b171427019a0012edf23963fba615d1e784db50
Rainrider <Rainrider@users.noreply.github.com>
2016-05-16 17:11:52 +0200

    Damn tab settings

--- data/Demonhunter.lua
+++ data/Demonhunter.lua
@@ -21,50 +21,50 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
 lib:__RegisterSpells("DEMONHUNTER", "70000", 1, {
-    COOLDOWN = {
-        185123, -- Throw Glaive (Havoc)
-        189110, -- Infernal Strike
-        195072, -- Fel Rush
-        [183752] = "INTERRUPT", -- Consume Magic
-        AURA = {
-            HELPFUL = {
-                [209426] = "SURVIVAL", -- Darkness
-            },
-            HARMFUL = {
-                 204598, -- Sigil of Flame
-                 204843, -- Sigil of Chains
-                 207407, -- Soul Carver (Vengeance artifact)
-                 200166, -- Metamorphosis (Havoc) (stun)
-                [204490] = "INTERRUPT", -- Sigil of Silence
-                [207744] = "SURVIVAL", -- Fiery Brand
-                [217832] = "CROWD_CTL", -- Imprison
-            },
-            PERSONAL = {
-                162264, -- Metamorphosis (Havoc)
-                178740, -- Immolation Aura
-                188499, -- Blade Dance
-                188501, -- Spectral Sight
-                210152, -- Death Sweep
-                SURVIVAL = {
-                    187827, -- Metamorphosis (Vengeance)
-                    203819, -- Demon Spikes
-                    212800, -- Blur
-                    218256, -- Empower Wards
-                },
-            },
-        },
-    },
+	COOLDOWN = {
+		185123, -- Throw Glaive (Havoc)
+		189110, -- Infernal Strike
+		195072, -- Fel Rush
+		[183752] = "INTERRUPT", -- Consume Magic
+		AURA = {
+			HELPFUL = {
+				[209426] = "SURVIVAL", -- Darkness
+			},
+			HARMFUL = {
+				 204598, -- Sigil of Flame
+				 204843, -- Sigil of Chains
+				 207407, -- Soul Carver (Vengeance artifact)
+				 200166, -- Metamorphosis (Havoc) (stun)
+				[204490] = "INTERRUPT", -- Sigil of Silence
+				[207744] = "SURVIVAL", -- Fiery Brand
+				[217832] = "CROWD_CTL", -- Imprison
+			},
+			PERSONAL = {
+				162264, -- Metamorphosis (Havoc)
+				178740, -- Immolation Aura
+				188499, -- Blade Dance
+				188501, -- Spectral Sight
+				210152, -- Death Sweep
+				SURVIVAL = {
+					187827, -- Metamorphosis (Vengeance)
+					203819, -- Demon Spikes
+					212800, -- Blur
+					218256, -- Empower Wards
+				},
+			},
+		},
+	},
 }, {
-    -- map aura to provider
-    [162264] = 191427, -- Metamorphosis (Havoc)
-    [200166] = 191427, -- Metamorphosis (Havoc) (stun)
-    [203819] = 203720, -- Demon Spikes
-    [204490] = 202137, -- Sigil of Silence
-    [204598] = 204596, -- Sigil of Flame
-    [204843] = 202138, -- Sigil of Chains
-    [207744] = 204021, -- Fiery Brand
-    [209426] = 196718, -- Darkness
-    [212800] = 198589, -- Blur
+	-- map aura to provider
+	[162264] = 191427, -- Metamorphosis (Havoc)
+	[200166] = 191427, -- Metamorphosis (Havoc) (stun)
+	[203819] = 203720, -- Demon Spikes
+	[204490] = 202137, -- Sigil of Silence
+	[204598] = 204596, -- Sigil of Flame
+	[204843] = 202138, -- Sigil of Chains
+	[207744] = 204021, -- Fiery Brand
+	[209426] = 196718, -- Darkness
+	[212800] = 198589, -- Blur
 }, {
-    -- map aura to modified spell(s)
+	-- map aura to modified spell(s)
 })
--- data/Hunter.lua
+++ data/Hunter.lua
@@ -21,72 +21,72 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
 lib:__RegisterSpells("HUNTER", "70000", 1, {
-    COOLDOWN = {
-            781, -- Disengage
-           1543, -- Flare
-          34026, -- Kill Command
-         109304, -- Exhilaration
-        [147362] = "INTERRUPT", -- Counter Shot
-        AURA = {
-            HARMFUL = {
-                  5116, -- Concussive Shot
-                224729, -- Bursting Shot
-            },
-            PERSONAL = {
-                   5384, -- Feign Death
-                  35079, -- Misdirection
-                 186257, -- Aspect of the Cheetah (90%)
-                 186258, -- Aspect of the Cheetah (30%)
-                 193530, -- Aspect of the Wild
-                 120694, -- Dire Beast
-                [ 19574] = "BURST", -- Bestial Wrath
-                [186265] = "SURVIVAL", -- Aspect of the Turtle
-                [193526] = "BURST", -- Trueshot
+	COOLDOWN = {
+			781, -- Disengage
+		   1543, -- Flare
+		  34026, -- Kill Command
+		 109304, -- Exhilaration
+		[147362] = "INTERRUPT", -- Counter Shot
+		AURA = {
+			HARMFUL = {
+				  5116, -- Concussive Shot
+				224729, -- Bursting Shot
+			},
+			PERSONAL = {
+				   5384, -- Feign Death
+				  35079, -- Misdirection
+				 186257, -- Aspect of the Cheetah (90%)
+				 186258, -- Aspect of the Cheetah (30%)
+				 193530, -- Aspect of the Wild
+				 120694, -- Dire Beast
+				[ 19574] = "BURST", -- Bestial Wrath
+				[186265] = "SURVIVAL", -- Aspect of the Turtle
+				[193526] = "BURST", -- Trueshot
 
-            },
-            PET = {
-                   136, -- Mend Pet
-                118455, -- Beast Cleave
-            },
-        },
-    },
-    AURA = {
-        HARMFUL = {
-              1462, -- Beast Lore (NOTE: HELPFUL on player controlled pets, HARMFUL else)
-              1515, -- Tame Beast
-            187131, -- Vulnurable
-        },
-        PERSONAL = {
-              6197, -- Eagle Eye
-            185365, -- Hunter's Mark
-            223138, -- Marking Targets
-        },
-    },
+			},
+			PET = {
+				   136, -- Mend Pet
+				118455, -- Beast Cleave
+			},
+		},
+	},
+	AURA = {
+		HARMFUL = {
+			  1462, -- Beast Lore (NOTE: HELPFUL on player controlled pets, HARMFUL else)
+			  1515, -- Tame Beast
+			187131, -- Vulnurable
+		},
+		PERSONAL = {
+			  6197, -- Eagle Eye
+			185365, -- Hunter's Mark
+			223138, -- Marking Targets
+		},
+	},
 }, {
-    -- map aura to provider(s)
-    [ 35079] = 34477, -- Misdirection
-    [118455] = 2643, -- Beast Cleave
-    [120694] = 120679, -- Dire Beast
-    [185365] = { -- Hunter's Mark
-          2643, -- Multi-Shot
-        185358, -- Arcane Shot
-    },
-    [186258] = 186257, -- Aspect of the Cheetah
-    [187131] = 185901, -- Vulnurable <- Marked Shot
-    [223138] = 75, -- Marking Targets <- Auto Shot
-    [224729] = 186387, -- Bursting Shot
+	-- map aura to provider(s)
+	[ 35079] = 34477, -- Misdirection
+	[118455] = 2643, -- Beast Cleave
+	[120694] = 120679, -- Dire Beast
+	[185365] = { -- Hunter's Mark
+		  2643, -- Multi-Shot
+		185358, -- Arcane Shot
+	},
+	[186258] = 186257, -- Aspect of the Cheetah
+	[187131] = 185901, -- Vulnurable <- Marked Shot
+	[223138] = 75, -- Marking Targets <- Auto Shot
+	[224729] = 186387, -- Bursting Shot
 }, {
-    -- map aura to modified spell(s)
-    [187131] = { -- Vulnurable
-         19434, -- Aimed Shot
-        185901, -- Marked Shot
-    },
-    [193526] = { -- Trueshot
-          2643, -- Multi-Shot
-        185358, -- Arcane Shot
-    },
-    [223138] = { -- Marking Targets
-          2643, -- Multi-Shot
-        185358, -- Arcane Shot
-    },
+	-- map aura to modified spell(s)
+	[187131] = { -- Vulnurable
+		 19434, -- Aimed Shot
+		185901, -- Marked Shot
+	},
+	[193526] = { -- Trueshot
+		  2643, -- Multi-Shot
+		185358, -- Arcane Shot
+	},
+	[223138] = { -- Marking Targets
+		  2643, -- Multi-Shot
+		185358, -- Arcane Shot
+	},
 })
