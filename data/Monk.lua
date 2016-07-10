be8dfc54563163f5f140c550b87c0bae94afb9a6
Rainrider <Rainrider@users.noreply.github.com>
2016-07-10 15:28:32 +0200

    monk: windwalker core abilities

--- data/Monk.lua
+++ data/Monk.lua
@@ -37,12 +37,16 @@ lib:__RegisterSpells("MONK", 70000, 1, {
 				116841, -- Tiger's Lust
 			},
 			HARMFUL = {
+				 115080, -- Touch of Death
+				 --115804, -- Mortal Wounds FIXME: see LibPlayerSpells-1.0#83
 				 116189, -- Provoke (taunt)
-				 117952, -- Crackling Jade Lightning
 				 118635, -- Provoke
 				 119381, -- Leg Sweep (stun)
 				 121253, -- Keg Smash (slow)
+				 122470, -- Touch of Karma
+				 123586, -- Flying Serpent Kick (slow)
 				 123725, -- Breath of Fire -- TODO: check without artifact
+				 124280, -- Touch of Karma (dmg dot)
 				 196727, -- Provoke
 				 196733, -- Special Delivery (slow)
 				 213063, -- Dark Side of the Moon (BM artifact)
@@ -54,6 +58,8 @@ lib:__RegisterSpells("MONK", 70000, 1, {
 				116844, -- Ring of Peace
 				116847, -- Rushing Jade Wind
 				119085, -- Chi Torpedo
+				125174, -- Touch of Karma
+				137639, -- Storm, Earth and Fire
 				196739, -- Elusive Dance
 				213177, -- Swift as a Coursing River (BM artifact)
 				214373, -- Brew-Stache (BM artifact)
@@ -70,16 +76,30 @@ lib:__RegisterSpells("MONK", 70000, 1, {
 	},
 	AURA = {
 		HARMFUL = {
+			116095, -- Disable (slow)
+			116706, -- Disable (root)
+			117952, -- Crackling Jade Lightning
 			196608, -- Eye of the Tiger
+			228287, -- Mark of the Crane
+		},
+		PERSONAL = {
+			101546, -- Spinning Crane Kick
+			116768, -- Blackout Kick!
 		},
 	},
 }, {
 	-- map aura to provider(s)
+	-- [115804] = 107428, -- Mortal Wounds <- Rising Sun Kick FIXME: see LibPlayerSpells-1.0#83
 	[116189] = 115546, -- Provoke (taunt)
+	[116706] = 116095, -- Disable (root)
+	[116768] = 100780, -- Blackout Kick! <- Tiger Palm
 	[118635] = 115315, -- Provoke <- Summon Black Ox Statue
 	[119085] = 115008, -- Chi Torpedo
 	[120954] = 115203, -- Fortifying Brew
+	[123586] = 101545, -- Flying Serpent Kick (slow)
 	[123725] = 115181, -- Breath of Fire
+	[124280] = 122470, -- Touch of Karma (dmg dot)
+	[125174] = 122470, -- Touch of Karma
 	[196608] = 196607, -- Eye of the Tiger
 	[196727] = 132578, -- Provoke <- Invoke Niuzao, the Black Ox
 	[196733] = 196730, -- Special Delivery (slow)
@@ -89,10 +109,17 @@ lib:__RegisterSpells("MONK", 70000, 1, {
 	[213177] = 213161, -- Swift as a Coursing River (BM artifact)
 	[214373] = 214372, -- Brew-Stache (BM artifact)
 	[215479] = 115308, -- Ironskin Brew
+	[228287] = { -- Mark of the Crane
+		100780, -- Tiger Palm
+		100784, -- Blacout Kick
+		107428, -- Rising Sun Kick
+	},
 	[228563] = 196736, -- Blackout Combo
 }, {
 	-- map aura to modified spell(s)
+	[116768] = 100784, -- Blackout Kick! -> Blackout Kick
 	[118635] = 115546, -- Provoke
+	[137639] = 221771, -- Storm, Earth and Fire -> Storm, Earth and Fire: Fixate
 	[196608] = 100780, -- Eye of the Tiger
 	[196733] = { -- Special Delivery (slow)
 		115308, -- Ironskin Brew
@@ -110,6 +137,7 @@ lib:__RegisterSpells("MONK", 70000, 1, {
 		115308, -- Ironskin Brew
 		119582, -- Purifying Brew
 	},
+	[228287] = 101546, -- Mark of the Crane -> Spinning Crane Kick
 	[228563] = { -- Blackout Combo
 		100780, -- Tiger Palm
 		115181, -- Beath of Fire
