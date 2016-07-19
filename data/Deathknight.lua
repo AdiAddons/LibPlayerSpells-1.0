f79b44606fd017e76c6ec042d08555e1b8cc74ed
Rainrider <Rainrider@users.noreply.github.com>
2016-07-19 14:36:55 +0200

    death knight: add blood artifact

--- data/Deathknight.lua
+++ data/Deathknight.lua
@@ -23,6 +23,7 @@ if not lib then return end
 lib:__RegisterSpells("DEATHKNIGHT", 70000, 1, {
 	COOLDOWN = {
 		  61999, -- Raise Ally
+		 205233, -- Consumption (Blood artifact)
 		 221699, -- Blood Tap
 		[ 47528] = "INTERRUPT", -- Mind Freeze
 		[108199] = "CROWD_CTL", -- Gorefiend's Grasp (knockback)
@@ -44,6 +45,7 @@ lib:__RegisterSpells("DEATHKNIGHT", 70000, 1, {
 					 48707, -- Anti-Magic Shield
 					 55233, -- Vampiric Blood
 					 81256, -- Dancing Rune Weapon
+					193320, -- Umbilicus Eternus (Blood artifact)
 					194679, -- Rune Tap
 					194844, -- Bonestorm
 					205725, -- Anti-Magic Barrier
@@ -84,6 +86,7 @@ lib:__RegisterSpells("DEATHKNIGHT", 70000, 1, {
 	[ 81256] = 49028, -- Dancing Rune Weapon
 	[ 81141] = 81136, -- Crimson Scourge
 	[188290] = 43265, -- Death and Decay
+	[193320] = 193213, -- Umbilicus Eternus (Blood artifact)
 	[195181] = 195182, -- Bone Shield <- Marrowrend
 	[205725] = 205727, -- Anti-Magic Barrier
 	[206961] = 206960, -- Tremble Before Me (disorient)
@@ -94,6 +97,7 @@ lib:__RegisterSpells("DEATHKNIGHT", 70000, 1, {
 		49998, -- Death Strike
 	},
 	[ 81141] = 43265, -- Crimson Scourge -> Death and Decay
+	[193320] = 55233, -- Umbilicus Eternus (Blood artifact) -> Vampiric Blood
 	[205725] = 48707, -- Anti-Magic Barrier -> Anti-Magic Shell
 	[206961] = 43265, -- Tremble Before Me (disorient) -> Death and Decay
 })
