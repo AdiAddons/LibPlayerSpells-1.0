b092b486883eb2caba7bf2afcb3c140cfc1b4974
Adirelle <adirelle+github@gmail.com>
2016-07-23 12:32:13 +0200

    Another attempt.

--- .travis-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-which git
-which ssh
-git push git@git.wowace.com:wow/libplayerspells-1-0/mainline.git $TRAVIS_BRANCH $TRAVIS_TAG
---
+++ push-to-wowace.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -x
+which git
+which ssh
+git push git@git.wowace.com:wow/libplayerspells-1-0/mainline.git $TRAVIS_BRANCH $TRAVIS_TAG
