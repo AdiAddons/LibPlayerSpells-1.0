d790e14e37e2b00bf3bcf996d3366503e27dc00c
arcadepro <arcadepro@users.noreply.github.com>
2016-07-24 00:26:32 +0100

    warrior: add protection core abilities and talents (#94)

--- data/Warrior.lua
+++ data/Warrior.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("WARRIOR", 70000, 2, {
+lib:__RegisterSpells("WARRIOR", 70000, 3, {
 	COOLDOWN = {
 		INTERRUPT = {
 			6552, -- Pummel
@@ -29,38 +29,63 @@ lib:__RegisterSpells("WARRIOR", 70000, 2, {
 			PERSONAL = {
 				  1719, -- Battle Cry
 				 18499, -- Berserker Rage
+				 23920, -- Spell Reflect
+				107574, -- Avatar (Talent 45)
+				132404, -- Shield Block (Prot)
+				202289, -- Renewed Fury (Talent 45)
+				202164, -- Bounding Stride (Talent 60)
+				227744, -- Ravager (Talent 100)
 				227847, -- Bladestorm (Arms)
 				SURVIVAL = {
+					   871, -- Shield Wall (Prot)
+					 12975, -- Last Stand (Prot)
 					118038, -- Die by the Sword (Arms, Fury)
 				},
 			},
 			HELPFUL = {
-				97463, -- Commanding Shout
+				 97463, -- Commanding Shout
+				223658, -- Safeguard (Talent 30)
 			},
 			HARMFUL = {
+				  1160, -- Demoralizing Shout (Prot)
+				  7922, -- Warbringer Stun (Talent 15)
+				132168, -- Shockwave (Talent 15)
+				132169, -- Storm Bolt (Talent 15)
 				208086, -- Colossus Smash (Arms)
 			},
 		},
 	},
 	AURA = {
-		HELPFUL = {
-		},
 		HARMFUL = {
-			--115804, -- Mortal Strike
+			  6343, -- Thunder Clap (Slow)
 			105771, -- Charge Root
+			115767, -- Deep Wounds (Prot)
 		},
 		PERSONAL = {
-			188923, -- Cleave WirlWind
+			 32216, -- Victory Rush
+			188923, -- Cleave Whirlwind
+			122510, -- Ultimatum (Prot)(Talent 45)
+			190456, -- Ignore Pain (Prot)
+			204488, -- Focused Rage
 		},
 	},
 }, {
-	-- map aura to provider(s)
-	[208086] = 167105, -- Colossus Smash
-	[188923] =    845, -- Cleave
-	[105771] =    100, -- Charge Root
-	[ 97463] =  97462, -- Commanding Shout
-	--[115804] = 12294, -- Mortal Strike
+	-- Map [aura] to provider (button)
+	[  7922] =             198304, -- Warbringer Stun (Charge)
+	[ 32216] = { 34428, 202168, }, -- Victorious, Killing Blow => Victory Rush, Impending Victory
+	[ 97463] =              97462, -- Commanding Shout
+	[105771] =                100, -- Charge Root
+	[115767] =              20243, -- Devastate (Deep Wounds)
+	[122510] =             204488, -- Ultimatum
+	[132168] =              46968, -- Shockwave
+	[132169] =             107570, -- Storm Bolt
+	[132404] =               2565, -- Shield Block (Prot)
+	[188923] =                845, -- Cleave
+	[202164] =               6544, -- Bounding Stride
+	[208086] =             167105, -- Colossus Smash
+	[227744] =             228920, -- Ravager
+	[223658] =             198304, -- Safeguard
 }, {
 	-- map aura to modified spell(s)
-	[188923] = 1680, -- Cleave to WirlWind
+	[188923] =               1680, -- Cleave to Whirlwind
 })
