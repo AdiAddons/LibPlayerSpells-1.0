53a6f9672ea80327a7c62d43a57405a86841ede9
Adirelle <adirelle+github@gmail.com>
2016-07-23 16:08:58 +0200

    Fool-proofed deploy.sh.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cd $(basenamme $0)
+WOWACE_PROJECT="$1"
+DEPLOY_DIR="$TRAVIS_BUILD_DIR/deploy"
 
 # Setup SSH
-mkdir -p $HOME/.ssh
-cp wowace.hostkeys $HOME/.ssh/known_hosts
-openssl aes-256-cbc -K $encrypted_bfcd8b8ecc98_key -iv $encrypted_bfcd8b8ecc98_iv -in wowace.key.enc -out $HOME/.ssh/id_rsa -d
-chmod -R og= $HOME/.ssh
+mkdir -p "$HOME/.ssh"
+cp "$DEPLOY_DIR/wowace.hostkeys" "$HOME/.ssh/known_hosts"
+openssl aes-256-cbc -K "$encrypted_bfcd8b8ecc98_key" -iv "$encrypted_bfcd8b8ecc98_iv" -in "$DEPLOY_DIR/wowace.key.enc" -out "$HOME/.ssh/id_rsa" -d
+chmod -R og= "$HOME/.ssh"
 
 # Do push
-git push "git@git.wowace.com:wow/$1/mainline.git" --tags $TRAVIS_BRANCH
+git push "git@git.wowace.com:wow/${WOWACE_PROJECT}/mainline.git" --tags "$TRAVIS_BRANCH"
