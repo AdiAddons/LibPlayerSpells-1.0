f4297050a812351e93abf9d35d6cf191bcabffd5
Adirelle <adirelle@gmail.com>
2016-07-25 15:26:32 +0200

    Travis: avoid deployment from getting stuck because of SSH interaction.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,5 +7,8 @@ mkdir -p "$HOME/.ssh"
 openssl aes-256-cbc -K "$encrypted_bfcd8b8ecc98_key" -iv "$encrypted_bfcd8b8ecc98_iv" -in "$DEPLOY_DIR/wowace.key.enc" -out "$HOME/.ssh/id_rsa" -d
 chmod -R og= "$HOME/.ssh"
 
+# Force BatchMode for SSH
+export GIT_SSH_COMMAND="$(which ssh) -o 'BatchMode Yes'"
+
 # Do push
 git push "git@git.wowace.com:wow/${WOWACE_PROJECT}/mainline.git" --tags "$TRAVIS_BRANCH"
