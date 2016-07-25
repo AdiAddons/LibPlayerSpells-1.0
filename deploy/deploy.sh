808b77d3f638542d31dd12228e37b7029b459a62
Adirelle <adirelle@gmail.com>
2016-07-25 11:47:02 +0200

    Use the ssh_known_hosts addon of Travis to populate ~/.ssh/known_hosts.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,6 @@ DEPLOY_DIR="$TRAVIS_BUILD_DIR/deploy"
 
 # Setup SSH
 mkdir -p "$HOME/.ssh"
-cp "$DEPLOY_DIR/wowace.hostkeys" "$HOME/.ssh/known_hosts"
 openssl aes-256-cbc -K "$encrypted_bfcd8b8ecc98_key" -iv "$encrypted_bfcd8b8ecc98_iv" -in "$DEPLOY_DIR/wowace.key.enc" -out "$HOME/.ssh/id_rsa" -d
 chmod -R og= "$HOME/.ssh"
 
