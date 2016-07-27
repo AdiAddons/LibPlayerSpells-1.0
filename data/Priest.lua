6e81c806e5c9001085eb11d8905cf77eb7ed229b
evnhali <evnhali@gmail.com>
2016-07-27 11:06:27 -0700

    priest: add discipline core abilities

--- data/Priest.lua
+++ data/Priest.lua
@@ -20,4 +20,70 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("PRIEST", 70000, 1, {})
+lib:__RegisterSpells("PRIEST", 70000, 1, {
+  COOLDOWN = {
+     32375, -- Mass Dispel
+     34433, -- Shadowfiend
+     47540, -- Penance
+     62618, -- Power Word: Barrier
+     73325, -- Leap of Faith
+    DISPEL = {
+      [527] = "HELPFUL", -- Purify
+      [528] = "HARMFUL", -- Dispel Magic
+    },
+    AURA = {
+      PERSONAL = {
+        SURVIVAL = {
+             586, -- Fade
+        },
+        BURST = {
+           47536, -- Rapture
+        },
+        186478, -- Reparation
+      },
+      HELPFUL = {
+            17,   -- Power Word: Shield
+         81782,   -- Power Word: Barrier
+        SURVIVAL = {
+           33206,   -- Pain Suppression
+        },
+      },
+      HARMFUL = {
+        CROWD_CTL = {
+            8122,   -- Psychic Scream (disorient)        
+        },
+      },
+    },
+  },
+  AURA = {
+    HELPFUL = {
+      194384, -- Atonement
+        2096, -- Mind Vision
+      UNIQUE_AURA = {
+        111759, -- Levitate
+      },
+    },
+    HARMFUL = {
+         589, -- Shadow Word: Pain
+      187464, -- Shadow Mend
+      CROWD_CTL = {
+          9484, -- Shackle Undead (incapacitate)
+           605, -- Mind Control (incapacitate)
+      },
+    },
+  },
+}, 
+{ -- map aura to provider(s)
+  [194384] =  81749, -- Atonement
+  [ 81782] =  62618, -- Power Word: Barrier
+  [111759] =   1706, -- Levitate
+  [186478] =  47540, -- Reparation <- Penance
+  [187464] = 186263, -- Shadow Mend
+},
+{ -- map aura(s) to modified spell(s)
+  [194384] = { -- Atonement
+        200829, -- Plea
+        186263, -- Shadow Mend
+        194509, -- Power Word: Radiance
+  },
+})
