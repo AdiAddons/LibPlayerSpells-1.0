35377df697d7f30a55c52723d9a754199b3f5db9
Rainrider <Rainrider@users.noreply.github.com>
2016-07-29 15:37:29 +0200

    shaman: add restoration talents

--- data/Shaman.lua
+++ data/Shaman.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("SHAMAN", 70000, 7, {
+lib:__RegisterSpells("SHAMAN", 70000, 8, {
 	COOLDOWN = {
 		    556, -- Astral Recall
 		  17364, -- Stormstrike
@@ -32,6 +32,7 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 		 192249, -- Stone Elemental
 		 198067, -- Fire Elemental
 		 196884, -- Feral Lunge
+		 197995, -- Wellspring
 		 198103, -- Earth Elemental
 		[ 57994] = "INTERRUPT", -- Wind Shear
 		AURA = {
@@ -40,6 +41,7 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 				 98007, -- Spirit Link Totem
 				157384, -- Eye of the Storm (Primal Stone Elemental)
 				192082, -- Wind Rush
+				201633, -- Earthen Shield
 			},
 			HARMFUL = {
 				 51490, -- Thunderstorm (slow) (knockback)
@@ -58,10 +60,13 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 			},
 			PERSONAL = {
 				   58875, -- Spirit Walk
+				   73685, -- Unleash Life
 				   73920, -- Healing Rain
 				   79206, -- Spiritwalker's Grace
 				  108281, -- Ancestral Guidance
+				  114052, -- Ascendance (Restoration)
 				  118522, -- Elemental Blast: Critical Strike
+				  157504, -- Cloudburst Totem
 				  173183, -- Elemental Blast: Haste
 				  173184, -- Elemental Blast: Mastery
 				  187878, -- Crash Lightning
@@ -91,7 +96,8 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 	},
 	AURA = {
 		HELPFUL = {
-			546, -- Water Walking
+			   546, -- Water Walking
+			207400, -- Ancestral Vigor
 		},
 		HARMFUL = {
 			182387, -- Earthquake
@@ -133,6 +139,7 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 	[118522] = 117014, -- Elemental Blast: Critical Strike <- Elemental Blast
 	[118905] = 192058, -- Static Charge (stun) <- Lightning Surge Totem
 	[157384] = 157382, -- Eye of the Storm (Primal Stone Elemental)
+	[157504] = 157153, -- Cloudburst Totem
 	[173183] = 117014, -- Elemental Blast: Haste <- Elemental Blast
 	[173184] = 117014, -- Elemental Blast: Mastery <- Elemental Blast
 	[182387] = 61882, -- Earthquake (knockback) <- Earthquake Totem
@@ -145,9 +152,11 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 	[197385] = 197211, -- Fury of Air (slow)
 	[198249] = 198248, -- Elemental Healing (Enhancement artifact)
 	[198300] = 198299, -- Gathering Storms (Enhancement artifact)
+	[201633] = 198838, -- Earthen Shield <- Earthen Shield Totem
 	[201846] = 201845, -- Stormbringer
 	[202004] = 197992, -- Landslide
 	[202192] = 210643, -- Resonance Totem <- Totem Mastery
+	[207400] = 207401, -- Ancestral Vigor
 	[210652] = 210643, -- Storm Totem <- Totem Mastery
 	[210658] = 210643, -- Ember Totem <- Totem Mastery
 	[210659] = 210643, -- Tailwind Totem <- Totem Mastery
@@ -164,6 +173,9 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 	[114050] = { -- Ascendance
 		 51505, -- Lava Burst
 		114074, -- Lava Beam
+	},
+	[157504] = { -- Cloudburst Totem
+		201764, -- Recall Cloudburst Totem
 	},
 	[191877] = 188196, -- Power of Maelstrom (Elemental artifact) -> Lightning Bolt
 	[197209] = { -- Lightning Rod
@@ -186,6 +198,12 @@ lib:__RegisterSpells("SHAMAN", 70000, 7, {
 	[205495] = { -- Stormkeeper (Elemental artifact)
 		188196, -- Lightning Bolt
 		188443, -- Chain Lightning
+	},
+	[207400] = { -- Ancestral Vigor
+		 1064, -- Chain Heal
+		 8004, -- Healing Surge
+		61295, -- Riptide
+		77472, -- Healing Wave
 	},
 	[210714] = 196840, -- Icefury -> Frost Shock
 	[215785] = 60103, -- Hot Hand -> Lava Lash
