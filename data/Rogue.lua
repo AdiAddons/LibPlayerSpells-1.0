dea48fe1cde234e17ae45d06fed4bb9a69af3495
Rainrider <Rainrider@users.noreply.github.com>
2016-07-31 19:16:48 +0200

    rogue: add outlaw core abilities

--- data/Rogue.lua
+++ data/Rogue.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("ROGUE", 70000, 1, {
+lib:__RegisterSpells("ROGUE", 70000, 2, {
 	COOLDOWN = {
 		 1725, -- Distract
 		[1766] = "INTERRUPT", -- Kick
@@ -32,12 +32,15 @@ lib:__RegisterSpells("ROGUE", 70000, 1, {
 				 209786, -- Goremaw's Bite (Subtlety artifact) (slow)
 				[137619] = "BURST", -- Marked for Death
 				CROWD_CTL = {
-					 408, -- Kidney Shot (stun)
-					2094, -- Blind (disorient)
+					   408, -- Kidney Shot (stun)
+					  1776, -- Gouge (incapacitate)
+					  2094, -- Blind (disorient)
+					199804, -- Between the Eyes (stun)
 				},
 			},
 			PERSONAL = {
 				   2983, -- Sprint
+				  13877, -- Blade Flurry
 				  36554, -- Shadowstep
 				  57934, -- Tricks of the Trade
 				 152150, -- Death from Above
@@ -51,8 +54,10 @@ lib:__RegisterSpells("ROGUE", 70000, 1, {
 					 11327, -- Vanish
 					 31224, -- Cload of Shadows
 					197603, -- Embrace of Darkness (Subtlety artifact)
+					199754, -- Riposte
 				},
 				BURST = {
+					 13750, -- Adrenaline Rush
 					121471, -- Shadow Blades
 				}
 			},
@@ -63,6 +68,7 @@ lib:__RegisterSpells("ROGUE", 70000, 1, {
 
 		},
 		HARMFUL = {
+			185763, -- Pistol Shot (slow)
 			195452, -- Nightblade
 			206760, -- Night Terrors (slow)
 			222775, -- Strike from the Shadows (slow)
@@ -74,9 +80,16 @@ lib:__RegisterSpells("ROGUE", 70000, 1, {
 		},
 		PERSONAL = {
 			  1966, -- Feint
+			193356, -- Broadsides
+			193357, -- Shark Infested Waters
+			193358, -- Grand Melee
+			193359, -- True Bearing
 			193538, -- Alacity
+			195627, -- Oportunity
 			197496, -- Finality: Eviscerate (Subtlety artifact)
 			197498, -- Finality: Nightblade (Subtlety artifact)
+			199600, -- Buried Treasure
+			199603, -- Jolly Rogger
 			206237, -- Enveloping Shadows
 		},
 	},
@@ -84,11 +97,18 @@ lib:__RegisterSpells("ROGUE", 70000, 1, {
 	-- map aura to provider(s)
 	[ 11327] = 1856, -- Vanish
 	[185422] = 185313, -- Shadow Dance
+	[193356] = 193316, -- Broadsides <- Roll the Bones
+	[193357] = 193316, -- Shark Infested Waters <- Roll the Bones
+	[193358] = 193316, -- Grand Melee <- Roll the Bones
+	[193359] = 193316, -- True Bearing <- Roll the Bones
 	[193538] = 193539, -- Alacity
+	[195627] = 193315, -- Oportunity <- Saber Stash
 	[196958] = 196951, -- Strike from the Shadows (stun)
 	[197496] = 197406, -- Finality: Eviscerate (Subtlety artifact) <- Finality (Subtlety artifact)
 	[197498] = 197406, -- Finality: Nightblade (Subtlety artifact) <- Finality (Subtlety artifact)
 	[197603] = 197604, -- Embrace of Darkness (Subtlety artifact)
+	[199600] = 193316, -- Buried Treasure <- Roll the Bones
+	[199603] = 193316, -- Jolly Rogger <- Roll the Bones
 	[206760] = 195452, -- Night Terrors (slow) <- Nightblade
 	[209786] = 209782, -- Goremaw's Bite (Subtlety artifact) (slow)
 	[220901] = 209782, -- Goremaw's Bite (Subtlety artifact)
@@ -102,10 +122,15 @@ lib:__RegisterSpells("ROGUE", 70000, 1, {
 		195452, -- Nightblade
 		196819, -- Eviscerate
 	},
+	[195627] = 185763, -- Oportunity -> Pistol Shot
 	[196958] = 185438, -- Strike from the Shadows (stun) -> Shadowstrike
 	[197496] = 196819, -- Finality: Eviscerate (Subtlety artifact) -> Eviscerate
 	[197498] = 195452, -- Finality: Nightblade (Subtlety artifact) -> Nightblade
 	[197603] = 1784, -- Embrace of Darkness (Subtlety artifact) -> Stealth
+	[199603] = { -- Jolly Rogger
+		193315, -- Saber Slash
+		193316, -- Roll the Bones
+	},
 	[222775] = 185438, -- Strike from the Shadows (slow) -> Shadowstrike
 	[227151] = 185438, -- Death -> Shadowstrike
 })
