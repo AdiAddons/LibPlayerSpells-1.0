eb8d8fed327d561266d6eeed8f47501b12fec442
Rainrider <Rainrider@users.noreply.github.com>
2016-08-02 20:53:54 +0200

    priest: add discipline talents

--- data/Priest.lua
+++ data/Priest.lua
@@ -31,9 +31,13 @@ lib:__RegisterSpells("PRIEST", 70000, 3, {
 		  47540, -- Penance
 		  64843, -- Divine Hymn
 		  73325, -- Leap of Faith
+		 110744, -- Divine Star
+		 120517, -- Halo
+		 129250, -- Power Word: Solace
 		 205351, -- Shadow Word: Void
 		 205385, -- Shadow Clash
-		[200174] = "POWER_REGEN", -- Mindbender
+		[123040] = "MANA_REGEN", -- Mindbender
+		[200174] = "POWER_REGEN", -- Mindbender (Shadow)
 		DISPEL = {
 			[   527] = "HELPFUL", -- Purify
 			[ 32375] = "HARMFUL HELPFUL", -- Mass Dispel
@@ -41,11 +45,12 @@ lib:__RegisterSpells("PRIEST", 70000, 3, {
 		},
 		AURA = {
 			HELPFUL = {
-				   17, -- Power Word: Shield
-				33076, -- Prayer of Mending
-				47788, -- Guardian Spirit
-				62618, -- Power Word: Barrier
-				65081, -- Body and Soul
+				    17, -- Power Word: Shield
+				 33076, -- Prayer of Mending
+				 47788, -- Guardian Spirit
+				 62618, -- Power Word: Barrier
+				 65081, -- Body and Soul
+				121557, -- Angelic Feather
 				SURVIVAL = {
 					33206, -- Pain Suppression
 					81782, -- Power Word: Barrier
@@ -54,8 +59,10 @@ lib:__RegisterSpells("PRIEST", 70000, 3, {
 			HARMFUL = {
 				 14914, -- Holy Fire
 				 15487, -- Silence -- NOTE: non-players only INTERRUPT, special case
+				204263, -- Shininig Force (slow)
 				205065, -- Void Torrent (Shadow artifact)
 				205369, -- Mind Bomb
+				214621, -- Schism
 				CROWD_CTL = {
 					  8122, -- Psychic Scream (disorient)
 					 88625, -- Holy Word: Chastise (incapacitate)
@@ -85,10 +92,14 @@ lib:__RegisterSpells("PRIEST", 70000, 3, {
 			  41635, -- Prayer of Mending
 			  64844, -- Divine Hymn
 			  77489, -- Echo of Light
+			 152118, -- Clarity of Will
 			 186367, -- Prayer's Reprise
 			 194384, -- Atonement
 			 208772, -- Smite
-			[187464] = "INVERT_AURA", -- Shadow Mend
+			INVERT_AURA = {
+				187464, -- Shadow Mend
+				219521, -- Shadow Covenant
+			},
 			[111759] = "UNIQUE_AURA", -- Levitate
 		},
 		HARMFUL = {
@@ -96,6 +107,7 @@ lib:__RegisterSpells("PRIEST", 70000, 3, {
 			 15407, -- Mind Flay
 			 34914, -- Void Touch
 			 48045, -- Mind Sear
+			204213, -- Purge the Wicked
 			217673, -- Mind Spike
 			CROWD_CTL = {
 				  9484, -- Shackle Undead (incapacitate)
@@ -122,6 +134,7 @@ lib:__RegisterSpells("PRIEST", 70000, 3, {
 	[ 77489] =  77485, -- Echo of Light
 	[ 81782] =  62618, -- Power Word: Barrier
 	[111759] =   1706, -- Levitate
+	[121557] = 121536, -- Angelic Feather
 	[124430] = 162452, -- Shadowy Insight
 	[186367] =  33076, -- Prayer's Reprise <- Prayer of Mending
 	[186478] =  47540, -- Reparation <- Penance
@@ -131,13 +144,18 @@ lib:__RegisterSpells("PRIEST", 70000, 3, {
 	[194025] = 194024, -- Thrive in the Shadows (Shadow artifact)
 	[194384] =  81749, -- Atonement
 	[200196] =  88625, -- Holy Word: Chastise
+	[204213] = 204197, -- Purge the Wicked
 	[205372] = 205371, -- Void Ray
 	[208772] = 585, -- Smite
 	[212570] = 193223, -- Surrendered Soul <- Surrender to Madness
 	[217673] = 73510, -- Mind Spike
+	[219521] = 204065, -- Shadow Covenant
 	[226943] = 205369, -- Mind Bomb (stun)
 }, { -- map aura(s) to modified spell(s)
-	[ 65081] =  17, -- Body and Soul -> Power Word: Shield
+	[ 65081] = { -- Body and Soul
+		   17, -- Power Word: Shield
+		73325, -- Leap of Faith
+	},
 	[ 77489] = { -- Echo of Light
 		  139, -- Renew
 		  596, -- Prayer of Healing
