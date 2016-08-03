0933b0342bb3ab8c0d13cd3eda7dc8558f3a3f3c
Rainrider <Rainrider@users.noreply.github.com>
2016-08-03 14:13:21 +0200

    paladin: add protection core abilities

--- data/Paladin.lua
+++ data/Paladin.lua
@@ -20,11 +20,14 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("PALADIN", 70000, 2, {
+lib:__RegisterSpells("PALADIN", 70000, 3, {
 	COOLDOWN = {
+		  20271, -- Judgement
+		  31935, -- Avenger's Shield -- NOTE: non-player only INTERRUPT
 		  35395, -- Crusader Strike
+		  53595, -- Hammer of the Righteous
 		 184575, -- Blade of Justice
-		 198034, -- Devine Hammer
+		 198034, -- Divine Hammer
 		 210220, -- Holy Wrath
 		[ 96231] = "INTERRUPT", -- Rebuke
 		DISPEL = {
@@ -38,6 +41,7 @@ lib:__RegisterSpells("PALADIN", 70000, 2, {
 				[25771] = "INVERT_AURA", -- Forbearance
 				SURVIVAL = {
 					1022, -- Blessing of Protection
+					6940, -- Blessing of Sacrifice
 				},
 			},
 			HARMFUL = {
@@ -56,6 +60,7 @@ lib:__RegisterSpells("PALADIN", 70000, 2, {
 				},
 			},
 			PERSONAL = {
+				188370, -- Consecration (Protection)
 				217020, -- Zeal -- NOTE: the buff charges overlay the spell charges
 				221883, -- Divine Steed
 				BURST = {
@@ -64,6 +69,9 @@ lib:__RegisterSpells("PALADIN", 70000, 2, {
 				},
 				SURVIVAL = {
 					   642, -- Divine Shield
+					 31850, -- Ardent Defender
+					 86659, -- Guardian of Ancient Kings
+					132403, -- Shield of the Righteous
 					184662, -- Shield of Vengeance
 					205191, -- Eye for an Eye
 				},
@@ -85,19 +93,27 @@ lib:__RegisterSpells("PALADIN", 70000, 2, {
 	-- map aura to provider(s)
 	[ 25771] = { -- Forbearance
 		 633, -- Lay on Hands
-		 642, -- Devine Shield
+		 642, -- Divine Shield
 		1022, -- Blessing of Protection
 	},
 	[105421] = 115750, -- Blinding Light (disorient)
+	[132403] = 53600, -- Shield of the Righteous
+	[188370] = 26573, -- Consecration (Protection)
 	[197277] = 20271, -- Judgement
-	[204242] = 205228, -- Consecration
+	[204242] = { -- Consecration
+		 26573, -- Consecration (Protection)
+		205228, -- Consecration (Retribution)
+	},
 	[205290] = 205273, -- Wake of Ashes (Retribution artifact) (stun)
-	[221883] = 205656, -- Divine Steed
+	[221883] = { -- Divine Steed
+		190784, -- Divine Steed (Protection)
+		205656, -- Divine Steed (Retribution)
+	},
 	[223819] = 223817, -- Divine Purpose
 }, {
 	-- map aura to modified spell(s)
 	[223819] = { -- Divine Purpose
-		 53385, -- Devine Storm
+		 53385, -- Divine Storm
 		 85256, -- Templar's Verdict
 		202273, -- Seal of Light
 		210191, -- Word of Glory
