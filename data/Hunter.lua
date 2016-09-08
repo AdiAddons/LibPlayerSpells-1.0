3ebd68a28272d5f3c53d270cf58afddca8a0bad6
Rainrider <Rainrider@users.noreply.github.com>
2016-09-08 16:37:51 +0200

    hunter: add Aspect of the Chameleon

--- data/Hunter.lua
+++ data/Hunter.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("HUNTER", 70000, 6, {
+lib:__RegisterSpells("HUNTER", 70000, 7, {
 	COOLDOWN = {
 		   781, -- Disengage
 		  1543, -- Flare
@@ -104,6 +104,7 @@ lib:__RegisterSpells("HUNTER", 70000, 6, {
 			PERSONAL = {
 				   5384, -- Feign Death
 				  35079, -- Misdirection
+				  61648, -- Aspect of the Chameleon
 				 118922, -- Posthaste
 				 120694, -- Dire Beast
 				 160007, -- Updraft (Rylak)
