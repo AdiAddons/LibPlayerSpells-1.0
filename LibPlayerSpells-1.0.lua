8270966c5847553a899a6e8735542794bf7cfe29
Zealot <zealot0630@gmail.com>
2016-09-16 07:55:19 +0800

    Fix filter when anyOf is empty

--- LibPlayerSpells-1.0.lua
+++ LibPlayerSpells-1.0.lua
@@ -298,7 +298,7 @@ function lib:GetFlagTester(anyOf, include, exclude)
 		local includeMask, excludeMask = filters[include], filters[exclude]
 		local mask = bor(includeMask, excludeMask)
 		local expected = bxor(mask, excludeMask)
-		if anyOf then
+		if anyOfMask ~= 0 then
 			return function(flags)
 				return flags and band(flags, anyOfMask) ~= 0 and band(flags, mask) == expected
 			end
@@ -307,7 +307,7 @@ function lib:GetFlagTester(anyOf, include, exclude)
 				return flags and band(flags, mask) == expected
 			end
 		end
-	elseif anyOf then
+	elseif anyOfMask ~= 0 then
 		return function(flags)
 			return flags and band(flags, anyOfMask) ~= 0
 		end
