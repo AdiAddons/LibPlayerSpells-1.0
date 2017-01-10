0ef872d29da18dac9bcfff69b4d11d5e3752665e
Adirelle <adirelle+github@gmail.com>
2017-01-10 20:45:17 +0100

    Fixed the webhook request.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sS "https://www.wowace.com/api/projects/$1/package?token=$WOWACE_API_TOKEN"
+exec curl -XPOST -sS "https://www.wowace.com/api/projects/$1/package?token=$WOWACE_API_TOKEN" -d '{"ping":1}'
