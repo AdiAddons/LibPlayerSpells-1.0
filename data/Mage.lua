4adede5c953f2d3d723ec186e662978bf3b0f588
Rainrider <Rainrider@users.noreply.github.com>
2017-09-04 16:41:24 +0200

    mage: bump data version

--- data/Mage.lua
+++ data/Mage.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
-lib:__RegisterSpells("MAGE", 70200, 2, {
+lib:__RegisterSpells("MAGE", 70300, 1, {
 	COOLDOWN = {
 		   1953, -- Blink
 		  31687, -- Summon Water Elemental
