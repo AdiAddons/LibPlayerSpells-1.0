88479f10728449ee80821b3e223907c576f16ce7
Rainrider <Rainrider@users.noreply.github.com>
2018-04-26 20:48:42 +0200

    hunter: add BfA marksmanship talents

--- data/Hunter.lua
+++ data/Hunter.lua
@@ -23,8 +23,11 @@ if not lib then return end
 lib:__RegisterSpells("HUNTER", 80000, 1, {
 	COOLDOWN = {
 		   781, -- Disengage
-		 19434, -- Aimed Shot
+		 19434, -- Aimed Shot (Marksmanship)
 		109304, -- Exhilaration
+		120360, -- Barrage (Marksmanship talent)
+		198670, -- Piercing Shot (Marksmanship talent)
+		212431, -- Explosive Shot (Marksmanship talent)
 		257044, -- Rapid Fire (Marksmanship)
 		INTERRUPT = {
 			147362, -- Counter Shot (Marksmanship, Survival)
@@ -34,11 +37,15 @@ lib:__RegisterSpells("HUNTER", 80000, 1, {
 				34477, -- Misdirection
 			},
 			HARMFUL = {
+				117405, -- Binding Shot
 				132951, -- Flare
 				CROWD_CTRL = {
 					INCAPACITATE = {
 						3355, -- Freezing Trap
 					},
+					ROOT = {
+						117526, -- Binding Shot (Marksmanship talent)
+					}
 				},
 				SNARE = {
 					  5116, -- Concussive Shot (Marksmanship)
@@ -52,7 +59,10 @@ lib:__RegisterSpells("HUNTER", 80000, 1, {
 				118922, -- Posthaste (Marksmanship talent)
 				186257, -- Aspect of the Cheetah
 				186258, -- Aspect of the Cheetah
+				199483, -- Camouflage (Marksmanship talent)
 				260242, -- Precise Shots (Marksmanship)
+				260402, -- Double Tap (Marksmanship talent)
+				269576, -- Master Marksman (Marksmanship talent)
 				BURST = {
 					193526, -- Trueshot (Marksmanship)
 				},
@@ -63,26 +73,55 @@ lib:__RegisterSpells("HUNTER", 80000, 1, {
 		},
 	},
 	AURA = {
+		HARMFUL = {
+			131894, -- A Murder of Crows (Marksmanship talent)
+			257284, -- Hunter's Mark (Marksmanship talent)
+			271788, -- Serpent Sting (Marksmanship talent)
+		},
 		PERSONAL = {
 			  6197, -- Eagle Eye
+			193534, -- Steady Focus (Marksmanship talent)
+			194594, -- Lock and Load (Marksmanship talent)
 			257622, -- Trick Shots (Marksmanship)
+			260395, -- Lethal Shots (Marksmanship)
+			269502, -- Lethal Shots (Marksmanship talent)
 		},
 	},
 }, {
 	-- map aura to provider(s)
 	[  3355] = 187650, -- Freezing Trap
 	[ 35079] = 34477, -- Misdirection
-	[118922] = 781, -- Posthaste (Marksmanship talent) <- Disengage
+	[117405] = 109248, -- Binding Shot (Marksmanship talent)
+	[117526] = 109248, -- Binding Shot (Marksmanship talent)
+	[118922] = 109215, -- Posthaste (Marksmanship talent)
 	[132951] = 1543, -- Flare
 	[135299] = 187698, -- Tar Trap
 	[186258] = 186557, -- Aspect of the Cheetah
+	[193534] = 193533, -- Steady Focus (Marksmanship talent)
+	[194594] = 194595, -- Lock and Load (Marksmanship talent)
 	[257622] = 257621, -- Trick Shots (Marksmanship)
 	[260242] = 260240, -- Precise Shots (Marksmanship)
+	[260395] = 260393, -- Lethal Shots (Marksmanship talent)
+	[269502] = 260393, -- Lethal Shots (Marksmanship talent)
+	[269576] = 260309, -- Master Marksman (Marksmanship talent)
 }, {
 	-- map aura to modified spell(s)
+	[118922] = 781, -- Posthaste (Marksmanship talent) -> Disengage
+	[193534] = 56641, -- Steady Focus (Marksmanship talent) -> Steady Shot
+	[194594] = 19434, -- Lock and Load (Marksmanship talent) -> Aimed Shot
 	[257622] = { -- Trick Shots (Marksmanship)
 		 19434, -- Aimed Shot
 		257044, -- Rapid Fire
 	},
 	[260242] = 185358, -- Precise Shots (Marksmanship) -> Arcane Shot
+	[260395] = 257044, -- Lethal Shots (Marksmanship talent) -> Rapid Fire
+	[260402] = { -- Double Tap (Marksmanship talent)
+		 19434, -- Aimed Shot
+		257044, -- Rapid Fire
+	},
+	[269502] = 19434, -- Lethal Shots (Marksmanship talent) -> Aimed Shot
+	[269576] = { -- Master Marksman (Marksmanship talent)
+		185358, -- Arcane Shot
+		267620, -- Multi-Shot
+	},
 })
