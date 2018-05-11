148399845778710c47e06d5cda313fd53db36495
Rainrider <Rainrider@users.noreply.github.com>
2018-05-11 17:53:32 +0200

    demonhunter: add vengeance honor talents

--- data/Demonhunter.lua
+++ data/Demonhunter.lua
@@ -25,36 +25,53 @@ lib:__RegisterSpells("DEMONHUNTER", 80000, 1, {
 		 189110, -- Infernal Strike (Vengeance)
 		 212084, -- Fel Devastation (Vengeance talent)
 		[183752] = 'INTERRUPT', -- Consume Magic (Vengeance)
-		POWER_REGEN = {
-			232893, -- Felblade (Vengeance talent)
-			263642, -- Fracture (Vengeance talent)
-		},
 		AURA = {
 			HARMFUL = {
 				204598, -- Sigil of Flame (Vengeance)
 				204843, -- Sigil of Chains (Vengeance talent)
+				206891, -- Intimidated (Vengeance honor talent)
 				207744, -- Fiery Brand (Vengeance)
 				210003, -- Razor Spikes (Vengeance talent)
 				[204490] = 'INTERRUPT', -- Sigil of Silence (Vengeance)
 				CROWD_CTRL = {
 					[185245] = 'TAUNT', -- Torment (Vengeance)
-					[217832] = 'INCAPACITATE', -- Imprison
 					DISORIENT = {
 						207685, -- Sigil of Misery (Vengeance)
+					},
+					INCAPACITATE = {
+						217832, -- Imprison
+						221527, -- Imprison (honor talent)
+					},
+					STUN = {
+						205630, -- Illidan's Grasp (hold) (Vengeance honor talent)
+						208618, -- Illidan's Grasp (thrown) (Vengeance honor talent)
+						213491, -- Demonic Trample (Vengeance honor talent)
 					},
 				},
 			},
 			PERSONAL = {
 				188501, -- Spectral Sight (Vengeance)
+				205629, -- Demonic Trample (Vengeance honor talent)
 				POWER_REGEN = {
 					178740, -- Immolation Aura (Vengeance)
 				},
 				SURVIVAL = {
 					187827, -- Metamorphosis (Vengeance)
 					203819, -- Demon Spikes (Vengeance)
+					208796, -- Jagged Spikes (Vengeance honor talent)
 					263648, -- Soul Barrier (Vengeance talent)
 				},
 			},
+		},
+		DISPEL = {
+			MAGIC = {
+				[205604] = 'HELPFUL', -- Reverse Magic (Vengeance honor talent)
+				[205625] = 'PERSONAL', -- Cleaned by Flame (Vengeance honor talent) -- BUG: not in the spellbook -- NOTE: Immolation Aura is then the dispelling spell
+			},
+		},
+		POWER_REGEN = {
+			232893, -- Felblade (Vengeance talent)
+			263642, -- Fracture (Vengeance talent)
 		},
 	},
 	AURA = {
@@ -77,10 +94,14 @@ lib:__RegisterSpells("DEMONHUNTER", 80000, 1, {
 	[204490] = 202137, -- Sigil of Silence (Vengeange)
 	[204598] = 188499, -- Sigil of Flame (Vengeance)
 	[204843] = 202138, -- Sigil of Chains (Vengeance talent)
+	[206891] = 198589, -- Intimidated (Vengeance honor talent) <- Tormentor
 	[207685] = 207684, -- Sigil of Misery (Vengeance)
 	[207693] = 207697, -- Feast of Souls (Vengeance talent)
 	[207744] = 204021, -- Fiery Brand (Vengeance)
+	[208618] = 205630, -- Illidan's Grasp (thrown) (Vengeance honor talent)
+	[208796] = 205627, -- Jagged Spikes (Vengeance honor talent) -- BUG: not in the spellbook
 	[210003] = 209400, -- Razor Spikes (Vengeance talent)
+	[213491] = 205629, -- Demonic Trample (Vengeance honor talent)
 	[247456] = 247454, -- Frailty (Vengeance talent) <- Spirit Bomb
 	[268178] = 268175, -- Void Reaver (Vengeance talent)
 }, {
@@ -95,11 +116,14 @@ lib:__RegisterSpells("DEMONHUNTER", 80000, 1, {
 		204513, -- Sigil of Flame (with Concentrated Sigils talent) (Vengeance)
 		204596, -- Sigil of Flame (Vengeance)
 	},
+	[205630] = 208173, -- Illidan's Grasp (Vengeance honor talent) -> Illidan's Grasp: Throw
+	[206891] = 207029, -- Intimidated (Vengeance honor talent) <- Tormentor
 	[207685] = { -- Sigil of Misery (Vengeance)
 		202140, -- Sigil of Misery (with Concentrated Sigils talent) (Vengeance)
 		207684, -- Sigil of Misery (Vengeance)
 	},
 	[207693] = 228477, -- Feast of Souls (Vengeance talent) -> Soul Cleave
+	[208796] = 203720, -- Jagged Spikes (Vengeance honor talent) -> Demon Spikes
 	[210003] = 6603, -- Razor Spikes (Vengeance talent) -> Auto Attack
 	[268178] = 228477, -- Void Reaver (Vengeance talent) -> Soul Cleave
 })
