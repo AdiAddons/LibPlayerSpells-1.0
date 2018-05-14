3eca1b5bf94f029dcdecd34c29a2d7cd19df3275
VaporAPX <dougandnic@gmail.com>
2018-05-14 01:41:27 -0700

    Paladin: Add Retribution Abilities
    Ret Abilites and adjustment to protection Judgement ID.

--- data/Paladin.lua
+++ data/Paladin.lua
@@ -22,14 +22,17 @@ local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
 lib:__RegisterSpells("PALADIN", 80000, 1, {
 	COOLDOWN = {
-		20271, -- Judgement (Protection)
+		35395, -- Crusader Strike (Retribution)
+		53385, -- Divine Storm (Retribution)
 		53595, -- Hammer of the Righteous (Protection)
+	       184575, -- Blade of Justice (Retribution)
+	       275779, -- Judgement (Protection)
 		INTERRUPT = {
 			96321, -- Rebuke
 		},
 		DISPEL = {
 			HELPFUL = {
-				[213644] = "DISEASE POISON", -- Cleanse Toxins (Protection)
+				[213644] = "DISEASE POISON", -- Cleanse Toxins (Protection/Retribution)
 				[236186] = "DISEASE POISON", -- Cleansing Light (Protection/Retribution PvP Talent)
 			},
 		},
@@ -44,37 +47,44 @@ lib:__RegisterSpells("PALADIN", 80000, 1, {
 				},
 			},
 			HARMFUL = {
-				 31935,  -- Avenger's Shield (Protection)
+				 31935, -- Avenger's Shield (Protection)
 				196941, -- Judgement of Light (Protection Talent)
+				197277, -- Judgement (Retribution)
 				204079, -- Final Stand (Protection Talent / Taunt)
 				204301, -- Blessed Hammer (Protection Talent)
 			       [206891] = "UNIQUE_AURA", -- Inquisition (Protection PvP Talent)
 				CROWD_CTRL = {
-					[62124] = "TAUNT", -- Hand of Reckoning (Protection Taunt)
-					  [853] = "STUN",    -- Hammer of Justice
-					[20066] = "INCAPACITATE", -- Repentance 
+					  [853] = "STUN", -- Hammer of Justice
+				   	[20066] = "INCAPACITATE", -- Repentance 
+					[62124] = "TAUNT", -- Hand of Reckoning (Protection/Retribution Taunt)
 				       [105421] = "DISORIENT", -- Blinding Light (Protection Talent)
+				},
+				SNARE = {
+					183218 -- Hand of Hindrance (Retribution)
 				},
 			},
 			PERSONAL = {
 				152262, -- Seraphim (Protection Talent)
 				188370, -- Consecration (Protection)
 				276112, -- Divine Steed
-				BURST = {
-				      31884, -- Avenging Wrath
-				},
-				SURVIVAL = {
-					642, -- Divine Shield
-				      31850, -- Ardent Defender
-				      86659, -- Guardian of Ancient Kings
-				     132403, -- Shield of the Righteous (Protection)
-			             204150, -- Aegis of Light (Protection Talent)
-				},
+					BURST = {
+						31884, -- Avenging Wrath
+					},
+					SURVIVAL = {
+						  642, -- Divine Shield
+						31850, -- Ardent Defender
+						86659, -- Guardian of Ancient Kings
+					       132403, -- Shield of the Righteous (Protection)
+					       184662, -- Shield of Vengeance (Retribution)
+					       204150, -- Aegis of Light (Protection Talent)
+					},
 			},
 		},
 	},
 	AURA = {
 		HELPFUL = {
+			203538, -- Greater Blessing of Kings (Retribution)
+			203539, -- Greater Blessing of Wisdom (Retribution)
 		},
 		PERSONAL = {
 		},
@@ -85,14 +95,15 @@ lib:__RegisterSpells("PALADIN", 80000, 1, {
 	[132403] = 53600, -- Shield of the Righteous (Protection)
 	[188370] = 26573, -- Consecration (Protection)
 	[196941] = 275779, -- Judgement of Light (Protection Talent)
+	[197277] = 20271, -- Judgement (Retribution)
 	[204079] = 62124, -- Final Stand to Hand of Reckoning (Taunt / Protection Talent)
 	[204301] = 204019, -- Blessed Hammer (Protection Talent)
 	[206891] = 207028, -- Intimidated to Inquisition (Protection Honor Talent / Taunt)
 	[25771] = { -- Forbearance
 		633,    -- Lay on Hands
 		642,    -- Divine Shield
-	       1022,    -- Blessing of Protection
-		  },
+	   1022,    -- Blessing of Protection
+		},
 	[276112] = 190784, -- Divine Steed
 }, {
 	-- Map Aura to Modified Spell(s)
