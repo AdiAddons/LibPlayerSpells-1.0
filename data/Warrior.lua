1aa219801f9b2f831e0808664e3625401a9adbbf
VaporAPX <dougandnic@gmail.com>
2018-05-20 23:33:21 -0700

    Warrior: Protection talents

--- data/Warrior.lua
+++ data/Warrior.lua
@@ -22,37 +22,43 @@ local lib = LibStub("LibPlayerSpells-1.0")
 if not lib then return end
 lib:__RegisterSpells("WARRIOR", 80000, 1, {
 	COOLDOWN = {
-		 6544, -- Heroic Leap (Protection)
-		 6572, -- Revenge (Protection)
-		23922, -- Shield Slam (Protection)
-	       [ 6552] = "INTERRUPT", -- Pummel
+		  6544, -- Heroic Leap (Protection)
+		  6572, -- Revenge (Protection)
+		 23922, -- Shield Slam (Protection)
+		202168, -- Impending Victory (Protection talent)
+	       [  6552] = "INTERRUPT", -- Pummel
 		AURA = {
 			HELPFUL = {
 				  6673, -- Battle Shout (Protection)
 				 97463, -- Rallying Cry (Protection)
 				147833, -- Intervene (Protection)
+				223658, -- Safeguard (Protection talent)
 			},
 			HARMFUL = {
 				  1160, -- Demoralizing Shout (Protection)
 				105771, -- Charge (Protection)
+				275335, -- Punish (Protection talent)
 				CROWD_CTRL = {
 					[5246] = "DISORIENT", -- Intimidating Shout (Protection)
 					STUN = {
 						132168, -- Shockwave (Protection)
+						132169, -- Storm Bolt (Protection talent)
 					},
 					TAUNT = {
 						355, -- Taunt (Protection)
 					},
 				},
 				SNARE = {
-					6343, -- Thunderclap (Protection)
+					6343, -- Thunder Clap (Protection)
 				},
-
 			},
 			PERSONAL = {
 				 18499, -- Berserker Rage (Protection)
 				 23920, -- Spell Reflection (Protection)
 				132404, -- Shield Block (Protection)
+				202164, -- Bounding Stride (Protection talent)
+				227744, -- Ravager (Protection talent)
+				275337, -- Bolster (Protection talent)
  				BURST = {
 					107574, -- Avatar (Protection)
 				},
@@ -61,7 +67,6 @@ lib:__RegisterSpells("WARRIOR", 80000, 1, {
 					12975, -- Last Stand (Protection)
 				},
 			},
-
 		},
 	},
 	AURA = {
@@ -69,7 +74,10 @@ lib:__RegisterSpells("WARRIOR", 80000, 1, {
 			115767, -- Deep Wounds (Protection)
 		},
 		PERSONAL = {
-			32216, -- Victorious (Protection)
+			 32216, -- Victorious (Protection)
+			202573, -- Vengeance: Revenge (Protection talent)
+			202574, -- Vengeance: Ignore Pain (Protection talent)
+			202602, -- Into the Fray (Protection talent)
 			SURVIVAL = {
 				190456, -- Ignore Pain (Protection)
 			},
@@ -77,17 +85,34 @@ lib:__RegisterSpells("WARRIOR", 80000, 1, {
 	},
 }, {
 	-- Map aura to provider(s)
-	[ 32216] =  34428, -- Victory Rush (Protection)
+	[ 32216] = { -- Victorious (Protection)
+		 34428, -- Victory Rush (Protection)
+		202168, -- Impending Victory (Protection talent)
+	},
 	[ 97463] =  97462, -- Rallying Cry (Protection)
 	[105771] = 198304, -- Intercept - Charge (Protection)
 	[115767] = 115768, -- Deep Wounds (Protection)
 	[132168] =  46968, -- Shockwave (Protection)
+	[132169] = 107570, -- Storm Bolt (Protection talent)
 	[132404] =   2565, -- Shield Block (Protection)
 	[147833] = 198304, -- Intercept - Intervene (Protection)
+	[202164] = 202163, -- Bounding Stride (Protection talent)
+	[202573] = 202572, -- Vengeance: Revenge <- Vengeance (Protectiont talent)
+	[202574] = 202572, -- Vengeance: Ignore Pain <- Vengeance (Protection talent)
+	[202602] = 202603, -- Into the Fray (Protection talent)
+	[223658] = 223657, -- Safeguard (Protection talent)
+	[227744] = 228920, -- Ravager (Protection talent)
+	[275335] = 275334, -- Punish (Protection talent)
 }, {
 	-- map aura to modified spell(s)
 	[115767] = { -- Deep Wounds (Protection)
-		 6572, -- Revenge (Protection)
-		20243, -- Devestate (Protection)
+		  6572, -- Revenge (Protection)
+		 20243, -- Devestate (Protection)
+		236279, -- Devastator (Protection talent)
 	},
+	[202164] =   6544, -- Bounding Stride -> Heroic Leap (Protection talent)
+	[202573] =   6572, -- Vengeance: Revenge -> Revenge (Protection talent)
+	[202574] = 190456, -- Vengeance: Ignore Pain -> Ignore Pain (Protection talent)
+	[223658] = 198304, -- Safeguard -> Intercept (Protection talent)
+	[275335] =  23922, -- Punish -> Shield Slam (Protection talent)
 })
