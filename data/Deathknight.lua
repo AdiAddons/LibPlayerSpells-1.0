7a5e5771f57ca1f833cbf411390e117250daefaf
Rainrider <Rainrider@users.noreply.github.com>
2018-05-24 14:36:25 +0200

    deathknight: update for Battle for Azeroth  (#128)

--- data/Deathknight.lua
+++ data/Deathknight.lua
@@ -18,223 +18,221 @@ You should have received a copy of the GNU General Public License
 along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-local lib = LibStub("LibPlayerSpells-1.0")
+local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
-lib:__RegisterSpells("DEATHKNIGHT", 70300, 1, {
+lib:__RegisterSpells('DEATHKNIGHT', 80000, 1, {
 	COOLDOWN = {
-		  47568, -- Empower Rune Weapon
-		  49206, -- Summon Gargoyle
-		  50997, -- Death Gate
-		  57330, -- Horn of Winter
-		  61999, -- Raise Ally (battle res)
-		 194913, -- Glacial Advance
-		 205233, -- Consumption (Blood artifact)
-		 220143, -- Apocalypse (Unholy artifact)
-		 221699, -- Blood Tap
-		[ 47528] = "INTERRUPT", -- Mind Freeze
-		[108199] = "KNOCKBACK", -- Gorefiend's Grasp (knockback)
+		  46584, -- Raise Dead (Unholy)
+		  49206, -- Summon Gargoyle (Unholy talent)
+		  49576, -- Death Grip
+		  50977, -- Death Gate
+		  61999, -- Raise Ally
+		 194913, -- Glacial Advance (Frost talent)
+		 210764, -- Rune Strike (Blood talent)
+		 274156, -- Consumption (Blood talent)
+		 275699, -- Apocalypse (Unholy)
+		[ 47528] = 'INTERRUPT', -- Mind Freeze
+		[108199] = 'KNOCKBACK', -- Gorefiend's Grasp (Blood)
 		AURA = {
-			HELPFUL = {
-				238698, -- Vampiric Aura (Blood artifact)
-			},
 			HARMFUL = {
-				  51399, -- Death Grip (taunt)
-				 130736, -- Soul Reaper
-				 206931, -- Blooddrinker
-				[206891] = "UNIQUE_AURA", -- Intimidated (PvP)
-				[206977] = "SURVIVAL", -- Blood Mirror
+				 47476, -- Strangulate (Blood honor talent) -- NOTE: Silence
+				 55078, -- Blood Plague (Blood)
+				 77606, -- Dark Simulacrum (Blood/Unholy honor talent)
+				115994, -- Unholy Blight (Unholy talent)
+				130736, -- Soul Reaper (Unholy talent)
+				203173, -- Death Chain (Blood honor talent)
+				206891, -- Intimidated (Blood honor talent)
+				206931, -- Blooddrinker (Blood talent)
+				206940, -- Mark of Blood (Blood talent)
+				211794, -- Winter is Comming (Frost talent)
+				212610, -- Walking Dead (Blood honor talent)
 				CROWD_CTRL = {
-					[56222] = "TAUNT", -- Dark Command (taunt)
-					DISORIENT = {
-						206961, -- Tremble Before Me (disorient)
-						207167, -- Blinding Sleet (disorient)
-					},
+					[207167] = 'DISORIENT', -- Blinding Sleet (Frost talent)
 					ROOT = {
-						 91807, -- Shambling Rush (ghoul) (root)
-						212540, -- Flesh Hook (abomination) (root)
+						 91807, -- Shambling Rush (Ghoul) (Unholy)
+						207171, -- Winter is Comming (Frost talent)
+						233395, -- Frozen Center (Frost honor talent)
 					},
 					STUN = {
-						 91797, -- Monstrous Blow (ghoul) (stun)
-						 91800, -- Gnaw (ghoul) (stun)
-						108194, -- Asphyxiate (Unholy talent) (stun)
-						212332, -- Smash (abomination) (stun)
-						212337, -- Powerful Smash (abomination) (stun)
-						221562, -- Asphyxiate (Blood) (stun)
+						 91797, -- Monstrous Blow (Ghoul) (Unholy)
+						 91800, -- Gnaw (Ghoul) (Unholy)
+						108194, -- Asphyxiate (Unholy talent)
+						221562, -- Asphyxiate (Blood)
 					},
+					TAUNT = {
+						51399, -- Death Grip (Blood)
+						56222, -- Dark Command
+					}
 				},
 				SNARE = {
-					190780, -- Frost Breath (Frost artifact) (slow)
-					191719, -- Gravitational Pull (Unholy artifact) (slow)
-					211793, -- Remorseless Winter (slow)
+					204206, -- Chilled (Frost honor talent)
+					206930, -- Heart Strike (Blood)
+					211793, -- Remorsless Winter (Frost)
+					273977, -- Grip of the Dead (Blood talent)
 				},
 			},
+			HELPFUL = {
+				[145622] = 'SURVIVAL', -- Anti-Magic Zone (honor talent)
+			},
 			PERSONAL = {
-				 51271, -- Pillar of Frost
-				152279, -- Breath of Sindragosa
-				196770, -- Remorseless Winter
-				188290, -- Death and Decay
-				194918, -- Blighted Rune Weapon
-				207127, -- Hungering Rune Weapon
-				207256, -- Obliteration
-				212552, -- Wraith Walk
-				215711, -- Soul Reaper
-				216974, -- Necrosis
-				218100, -- Defile
-				[42650] = "BURST", -- Army of the Dead
+				  48265, -- Death's Advance
+				  77535, -- Blood Shield (Blood)
+				 115989, -- Unholy Blight (Unholy talent)
+				 152279, -- Breath of Sindragosa (Frost talent)
+				 188290, -- Death and Decay (Blood/Unholy)
+				 194844, -- Bonestorm (Blood talent)
+				 195181, -- Bone Shield (Blood)
+				 196770, -- Remorseless Winter (Frost)
+				 212552, -- Wraith Walk (talent)
+				 215711, -- Soul Ripper (Unholy talent)
+				 219788, -- Ossuary (Blood talent)
+				 273947, -- Hemostasis (Blood talent)
+				 274009, -- Voracious (Blood talent)
+				[ 48743] = 'INVERT_AURA', -- Death Pact (Unholy talent)
+				[219809] = 'POWER_REGEN', -- Tombstone (Blood talent)
+				BURST = {
+					 42650, -- Army of the Dead (Unholy)
+					 51271, -- Pillar of Frost (Frost)
+					207256, -- Obliteration (Frost talent)
+					207289, -- Unholy Frenzy (Unholy talent)
+				},
 				SURVIVAL = {
-					 48707, -- Anti-Magic Shield
+					 48707, -- Anti-Magic Shell
 					 48792, -- Icebound Fortitude
-					 55233, -- Vampiric Blood
-					 81256, -- Dancing Rune Weapon
-					193320, -- Umbilicus Eternus (Blood artifact)
-					194679, -- Rune Tap
-					194844, -- Bonestorm
-					205725, -- Anti-Magic Barrier
-					207319, -- Corpse Shield
-					219809, -- Tombstone
+					 55233, -- Vampiric Blood (Blood)
+					 81256, -- Dancing Rune Weapon (Blood)
+					194679, -- Rune Tap (Blood talent)
 				},
 			},
 			PET = {
-				 63560, -- Dark Transformation
-				 91837, -- Putrid Bullwark (ghoul)
-				 91838, -- Huddle (ghoul)
-				212383, -- Gastric Bloat (abomination)
-				212385, -- Protective Bile (abomination)
+				63560, -- Dark Transformation (Unholy)
+				SURVIVAL = {
+					91837, -- Putrid Bulwark (Ghoul) (Unholy)
+					91838, -- Huddle (Ghoul) (Unholy)
+				}
 			},
 		},
+		POWER_REGEN = {
+			 47568, -- Empower Rune Weapon (Frost)
+			 57330, -- Horn of Winter (Frost talent)
+			207127, -- Hungering Rune Weapon (Frost talent)
+		}
 	},
 	AURA = {
+		HARMFUL = {
+			  55095, -- Frost Fever (Frost)
+			 191587, -- Virulent Plague (Unholy)
+			 194310, -- Festering Wound (Unholy)
+			 196782, -- Outbreak (Unholy)
+			 199969, -- Wandering Plague (Unholy honor talent)
+			 223929, -- Necrotic Wound (Unholy honor talent)
+			 233397, -- Delirium (Frost honor talent)
+			[ 45524] = 'SNARE', -- Chains of Ice (Frost/Unholy)
+			CROWD_CTRL = {
+				[204085] = 'ROOT', -- Deathchill (Frost honor talent)
+				STUN = {
+					207165, -- Abomination's Might (Frost talent)
+					210141, -- Zombie Explosion (Unholy honor talent)
+				},
+			},
+		},
 		HELPFUL = {
 			3714, -- Path of Frost
 		},
-		HARMFUL = {
-			 55078, -- Blood Plague
-			 55095, -- Frost Fever
-			191587, -- Virulent Plague
-			194310, -- Festering Wound
-			196782, -- Outbreak
-			206940, -- Mark of Blood
-			208278, -- Debilitating Infestation
-			211794, -- Winter is Coming
-			CROWD_CTRL = {
-				STUN = {
-					207165, -- Abomination's Might (stun)
-					207171, -- Winter is Coming (stun)
-				},
-			},
-			SNARE = {
-				 45524, -- Chains of Ice (slow)
-				206930, -- Hearth Strike (slow)
-				211831, -- Abomination's Might (slow)
-			},
-		},
 		PERSONAL = {
-			 51124, -- Killing Machine
-			 59052, -- Rime
-			 77535, -- Blood Shield
-			 81141, -- Crimson Scourge
-			 81340, -- Sudden Doom
-			101568, -- Dark Succor
-			194879, -- Icy Talons
-			195181, -- Bone Shield
-			207203, -- Frost Shield
-			240558, -- Souldrinker (Blood artifact)
-			253595, -- Inexorable Assault
+			 51124, -- Killing Machine (Frost)
+			 59052, -- Rime (Frost)
+			 81141, -- Crimson Scourge (Blood)
+			 81340, -- Sudden Doom (Unholy)
+			101568, -- Dark Succor (Frost/Unholy)
+			194879, -- Icy Talons (Frost talent)
+			207203, -- Frost Shield (Frost talent)
+			233411, -- Blood for Blood (Blood honor talent)
 		},
 		PET = {
-			[111673] = "INVERT_AURA", -- Control Undead
+			[111673] = 'INVERT_AURA', -- Control Undead
 		},
 	},
 }, {
 	-- map aura to provider(s)
-	[ 51124] = 51128, -- Killing Machine
-	[ 51399] = 49576, -- Death Grip (taunt)
-	[ 55078] = { -- Blood Plague
+	[ 51124] =  51128, -- Killing Machine (Frost)
+	[ 51399] = 206930, -- Death Grip (Blood) <- Heart Strike -- NOTE: to signify the taunt is Blood only
+	[ 55078] = { -- Blood Plague (Blood)
 		 50842, -- Blood Boil
 		195292, -- Death's Caress
 	},
-	[ 55095] = 49184, -- Frost Ferver -> Howling Blast
-	[ 59052] = 59057, -- Rime
-	[ 77535] = 77513, -- Blood Shield <- Mastery: Blood Shield
-	[ 81256] = 49028, -- Dancing Rune Weapon
-	[ 81340] = 49530, -- Sudden Doom
-	[ 81141] = 81136, -- Crimson Scourge
-	[ 91797] = 63560, -- Monstrous Blow (ghoul) (stun) <- Dark Transformation
-	[ 91800] = 47481, -- Gnaw (ghoul) (stun)
-	[ 91807] = 63560, -- Shambling Rush (ghoul) (root) <- Dark Transformation
-	[ 91837] = 63560, -- Putrid Bullwark <- Dark Transformation
-	[ 91838] = 47484, -- Huddle
-	[101568] = 178819, -- Dark Succor
-	[188290] = { -- Death and Decay
-		 43265, -- Death and Decay
-		152280, -- Defile
+	[ 55095] =  49184, -- Frost Fever (Frost) <- Howling Blast
+	[ 59052] =  59057, -- Rime (Frost)
+	[ 77535] =  77513, -- Blood Shield (Blood) <- Mastery: Blood Shield
+	[ 81141] =  81136, -- Crimson Scourge (Blood)
+	[ 81256] =  49028, -- Dancing Rune Weapon (Blood)
+	[ 81340] =  49530, -- Sudden Doom (Unholy)
+	[ 91797] =  63560, -- Monstrous Blow (Ghoul) (Unholy) <- Dark Transformation
+	[ 91800] =  47481, -- Gnaw (Ghoul) (Unholy)
+	[ 91807] =  63560, -- Shambling Rush (Ghoul) (Unholy) <- Dark Transformation
+	[ 91837] =  63560, -- Putrid Bulwark (Ghoul) (Unholy) <- Dark Transformation
+	[ 91838] =  47484, -- Huddle (Ghoul) (Unholy)
+	[101568] = 178819, -- Dark Succor (Frost/Unholy)
+	[145622] =  51052, -- Anti-Magic Zone (honor talent)
+	[115994] = 115989, -- Unholy Blight (Unholy talent)
+	[188290] = { -- Death and Decay (Blood/Unholy)
+		 43265, -- Death and Decay (Blood/Unholy)
+		152280, -- Defile (Unholy talent)
 	},
-	[190780] = 190778, -- Frost Breath (Frost artifact) (slow)
-	[191587] = 77575, -- Virulent Plague <- Outbreak
-	[191719] = 191721, -- Gravitational Pull (Unholy artifact) (slow)
-	[193320] = 193213, -- Umbilicus Eternus (Blood artifact)
-	[194310] = 85948, -- Festering Wound <- Festering Strike
-	[194879] = 194878, -- Icy Talons
-	[195181] = 195182, -- Bone Shield <- Marrowrend
-	[196782] = 77575, -- Outbreak
-	[205725] = 205727, -- Anti-Magic Barrier
-	[206891] = 207018, -- Intimidated <- Murderous Intent (Blood Honor Talent)
-	[206961] = 206960, -- Tremble Before Me (disorient)
-	[207165] = 207161, -- Abomination's Might (stun)
-	[207171] = 207170, -- Winter is Coming (stun)
-	[207203] = 207200, -- Frost Shield <- Permafrost
-	[208278] = 207316, -- Debilitating Infestation
-	[211793] = 196770, -- Remorseless Winter (slow)
-	[211794] = 207170, -- Winter is Coming
-	[211831] = 207161, -- Abomination's Might (slow)
-	[212332] = 212336, -- Smash (abomination) (stun)
-	[212337] = 63560, -- Powerful Smash (abomination) (stun) <- Dark Transformation
-	[212383] = 63560, -- Gastric Bloat (abomination) <- Dark Transformation
-	[212385] = 212384, -- Protective Bile (abomination)
-	[212540] = 63560, -- Flesh Hook (abomination) (root) <- Dark Transformation
-	[215711] = 130736, -- Soul Reaper
-	[216974] = 207346, -- Necrosis
-	[218100] = 152280, -- Defile
-	[238698] = 238078, -- Vampiric Aura (Blood artifact)
-	[240558] = 238114,-- Souldrinker (Blood artifact)
-	[253595] = 253593, -- Inexorable Assault
+	[191587] =  77575, -- Virulent Plague (Unholy) <- Outbreak
+	[194310] =  85948, -- Festering Wound (Unholy) <- Festering Strike
+	[194879] = 194878, -- Icy Talons (Frost talent)
+	[195181] = 195182, -- Bone Shield (Blood) <- Marrowrend
+	[204085] = 204080, -- Deathchill (Frost honor talent)
+	[211793] = 196770, -- Remorsless Winter (Frost)
+	[196782] =  77575, -- Outbreak (Unholy)
+	[199969] = 199725, -- Wandering Plague (Unholy honor talent)
+	[204206] = 204160, -- Chilled (Frost honor talent) <- Chill Streak
+	[206891] = 207018, -- Intimidated <- Murderous Intent (Blood honor talent)
+	[207165] = 207161, -- Abomination's Might (Frost talent)
+	[207171] = 207170, -- Winter is Comming (Frost talent)
+	[207203] = 207200, -- Frost Shield <- Permafrost (Frost talent)
+	[210141] = 210128, -- Zombie Explosion <- Reanimation (Unholy honor talent)
+	[211794] = 207170, -- Winter is Comming (Frost talent)
+	[212610] = 202731, -- Walking Dead (Blood honor talent)
+	[215711] = 130736, -- Soul Ripper (Unholy talent)
+	[219788] = 219786, -- Ossuary (Blood talent)
+	[223929] = 223829, -- Necrotic Wound (Unholy honor talent)
+	[233395] = 204135, -- Frozen Center (Frost honor talent)
+	[233397] = 233396, -- Delirium (Frost honor talent)
+	[273947] = 273946, -- Hemostasis (Blood talent)
+	[273977] = 273952, -- Grip of the Dead (Blood talent)
+	[274009] = 273953, -- Voracious (Blood talent)
 }, {
 	-- map aura to modified spell(s)
-	[ 51124] = { -- Killing Machine
+	[ 51124] = { -- Killing Machine (Frost)
 		 49020, -- Obliterate
-		207230, -- Frostscythe
+		207230, -- Frostscythe (Frost talent)
 	},
-	[ 59052] = 49184, -- Rime -> Howling Blast
-	[ 77535] = 49998, -- Blood Shield -> Death Strike
-	[ 81141] = 43265, -- Crimson Scourge -> Death and Decay
-	[ 81340] = 47541, -- Sudden Doom -> Death Coil
-	[ 91797] = 47481, -- Monstrous Blow (ghoul) (stun) -> Gnaw (with Dark Transformation)
-	[ 91807] = 47482, -- Shambling Rush (ghoul) (root) -> Leap (with Dark Transformation)
-	[ 91837] = 47484, -- Putrid Bullwark -> Huddle (with Dark Transformation)
-	[101568] = 49998, -- Dark Succor -> Death Strike
-	[188290] = { -- Death and Decay
-		 55090, -- Scourge Strike
-		207311, -- Clawing Shadows
+	[ 51399] =  49576, -- Death Grip (Blood)
+	[ 59052] =  49184, -- Rime (Frost) -> Howling Blast
+	[ 77535] =  49998, -- Blood Shield (Blood) -> Death Strike
+	[ 81141] =  43265, -- Crimson Scourge (Blood) -> Death and Decay
+	[ 81340] =  47541, -- Sudden Doom (Unholy) -> Death Coil
+	[ 91797] =  47481, -- Monstrous Blow (Ghoul) (Unholy) <- Gnaw (Ghoul)
+	[ 91807] =  47482, -- Shambling Rush (Ghoul) (Unholy) <- Leap (Ghoul)
+	[ 91837] =  47484, -- Putrid Bulwark (Ghoul) (Unholy) <- Huddle (Ghoul)
+	[101568] =  49998, -- Dark Succor (Frost/Unholy) -> Death Strike
+	[194879] =  49143, -- Icy Talons (Frost talent) -> Frost Strike
+	[199969] =  77575, -- Wandering Plague (Unholy honor talent) -> Outbreak
+	[207165] =  49020, -- Abomination's Might (Frost talent) -> Obliterate
+	[207171] = 196170, -- Winter is Comming (Frost talent) -> Remorseless Winter
+	[207203] =   6603, -- Frost Shield (Frost talent) -> Auto Attack
+	[204085] =  45524, -- Deathchill (Frost honor talent) -> Chains of Ice
+	[211794] = 196170, -- Winter is Comming (Frost talent) -> Remorseless Winter
+	[212610] =  49576, -- Walking Dead (Blood honor talent) -> Death Grip
+	[219788] = 195182, -- Ossuary (Blood talent) -> Marrowrend
+	[233395] = 196770, -- Frozen Center (Frost honor talent) -> Remorseless Winter
+	[233397] = { -- Delirium (Frost honor talent)
+		49143, -- Frost Strike
+		49184, -- Howling Blast
 	},
-	[191719] = 49576, -- Gravitational Pull (Unholy artifact) (slow) -> Death Grip
-	[193320] = 55233, -- Umbilicus Eternus (Blood artifact) -> Vampiric Blood
-	[194879] = 49143, -- Icy Talons -> Frost Strike
-	[205725] = 48707, -- Anti-Magic Barrier -> Anti-Magic Shell
-	[206961] = 43265, -- Tremble Before Me (disorient) -> Death and Decay
-	[207165] = 49020, -- Abomination's Might (stun) -> Obliterate
-	[207171] = 196770, -- Winter is Coming (stun) -> Remorseless Winter
-	[207203] = 6603, -- Frost Shield -> Auto Attack
-	[208278] = 77575, -- Debilitating Infestation -> Outbreak
-	[211794] = 196770, -- Winter is Coming -> Remorseless Winter
-	[211831] = 49020, -- Abomination's Might (slow) -> Obliterate
-	[212337] = 212336, -- Powerful Smash (abomination) (stun) -> Smash (with Dark Transformation)
-	[212383] = 212384, -- Gastric Bloat (abomination) -> Protective Bile (with Dark Transformation)
-	[212540] = 212468, -- Flesh Hook (abomination) (root) -> Hook (with Dark Transformation)
-	[216974] = { -- Necrosis
-		 55090, -- Scourge Strike
-		207311, -- Clawing Shadows
-	},
-	[238698] = 205223, -- Vampiric Aura -> Consumption (Blood artifact)
-	[240558] = 49998,-- Souldrinker (Blood artifact) -> Death Strike
-	[253595] = 6603, -- Inexorable Assault -> Auto Attack
+	[273947] =  49998, -- Hemostasis (Blood talent) -> Death Strike
+	[273977] =  43265, -- Grip of the Dead (Blood talent) -> Death and Decay
+	[274009] =  49998, -- Voracious (Blood talent) -> Death Strike
 })
