159ba2224c9d77af52b0c918148ea2c6d5a20671
Rainrider <Rainrider@users.noreply.github.com>
2018-06-25 23:12:09 +0200

    druid: add guardian baseline abilities

--- data/Druid.lua
+++ data/Druid.lua
@@ -21,9 +21,64 @@ along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
 local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
 lib:__RegisterSpells('DRUID', 80000, 1, {
-
+	COOLDOWN = {
+		  18960, -- Teleport: Moonglade
+		  20484, -- Rebirth
+		[106839] = 'INTERRUPT', -- Skull Bash (Guardian)
+		AURA = {
+			HARMFUL = {
+				164812, -- Moonfire (Guardian)
+				106830, -- Thrash (Guardian - Cat)
+				192090, -- Thrash (Guardian - Bear)
+				CROWD_CTRL = {
+					[ 339] = 'ROOT', -- Entangling Roots (Guardian)
+					[6795] = 'TAUNT', -- Growl (Guardian)
+					INCAPACITATE = {
+						  99, -- Incapacitating Roar (Guardian)
+						2637, -- Hibernate -- TODO: category
+					},
+				},
+			},
+			HELPFUL = {
+				77761, -- Stampeding Roar (Guardian)
+			},
+			PERSONAL = {
+				 1850, -- Dash (Feral/Guardian)
+				 5215, -- Prowl (Guardian)
+				22842, -- Frenzied Regeneration (Guardian)
+				93622, -- Gore (Guardian)
+				SURVIVAL = {
+					22812, -- Barkskin (Guardian)
+					61336, -- Survival Instincts (Guardian)
+				},
+			},
+		},
+		DISPEL = {
+			HELPFUL = {
+				[2782] = 'CURSE POISON', -- Remove Corruption (Balance/Feral/Guardian)
+			},
+		},
+		POWER_REGEN = {
+			33917, -- Mangle (Guardian)
+			77758, -- Thrash (Guardian - Bear)
+		},
+	},
+	AURA = {
+		HELPFUL = {
+			8936, -- Regrowth (Guardian)
+		},
+		PERSONAL = {
+			SURVIVAL = {
+				192081, -- Ironfur (Guardian)
+			},
+		},
+	},
 }, {
 	-- map aura to provider(s)
+	[ 93622] = 210706, -- Gore (Guardian)
+	[164812] =  8921, -- Moonfire (Guardian)
+	[192090] = 77758, -- Thrash (Guardian - Bear)
 }, {
 	-- map aura to modified spell(s)
+	[ 93622] = 33917, -- Gore (Guardian) -> Mangle
 })
