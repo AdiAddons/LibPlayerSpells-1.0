ca74a47a8a898f85d5dc88df7308a4f8ecce1b49
Rainrider <Rainrider@users.noreply.github.com>
2018-07-02 18:51:12 +0200

    mage: add frost talents

--- data/Mage.lua
+++ data/Mage.lua
@@ -25,28 +25,37 @@ lib:__RegisterSpells('MAGE', 80000, 1, {
 		   1953, -- Blink
 		  31687, -- Summon Water Elemental (Frost)
 		  84714, -- Frozen Orb (Frost)
+		 153595, -- Comet Storm (Frost talent)
 		 190336, -- Conjure Refreshment
 		 190356, -- Blizzard (Frost)
+		 212653, -- Shimmer (talent)
+		 257537, -- Ebonbolt (Frost talent)
 		[   475] = 'DISPEL HELPFUL CURSE', -- Remove Curse
 		[  2139] = 'INTERRUPT', -- Counterspell
 		[235219] = 'SURVIVAL', -- Cold Snap (Frost)
 		AURA = {
 			HARMFUL = {
 				CROWD_CTRL = {
+					[82691] = 'INCAPACITATE', -- Ring of Frost (Frost talent)
 					ROOT = {
-						  122, -- Frost Nova
-						33395, -- Freeze (Frost - Water Elemental)
+						   122, -- Frost Nova
+						 33395, -- Freeze (Frost - Water Elemental)
+						157997, -- Ice Nova (Frost talent)
 					},
 				},
 				SNARE = {
+					205021, -- Ray of Frost (Frost talent)
 					212792, -- Cone of Cold (Frost)
 				},
 			},
 			PERSONAL = {
-				 11426, -- Ice Barrier (Frost)
-				[41425] = 'INVERT_AURA', -- Hypothermia
+				  11426, -- Ice Barrier (Frost)
+				 108839, -- Ice Floes (Frost talent)
+				 270232, -- Freezing Rain (Frost talent)
+				[ 41425] = 'INVERT_AURA', -- Hypothermia
 				BURST = {
-					12472, -- Icy Veins (Frost)
+					 12472, -- Icy Veins (Frost)
+					116014, -- Rune of Power (Frost talent)
 				},
 				SURVIVAL = {
 					   66, -- Invisibility (Fading)
@@ -59,6 +68,7 @@ lib:__RegisterSpells('MAGE', 80000, 1, {
 	AURA = {
 		HARMFUL = {
 			CROWD_CTRL = {
+				[228600] = 'ROOT', -- Glacial Spike (Frost talent)
 				INCAPACITATE = {
 					   118, -- Polymorph (Sheep)
 					 28271, -- Polymorph (Turtle)
@@ -88,6 +98,8 @@ lib:__RegisterSpells('MAGE', 80000, 1, {
 			 44544, -- Fingers of Frost (Frost)
 			190446, -- Brain Freeze (Frost)
 			205473, -- Icicles (Frost)
+			205766, -- Bone Chilling (Frost talent)
+			278310, -- Chain Reaction (Frost talent)
 		},
 	},
 	DISPEL = {
@@ -98,6 +110,8 @@ lib:__RegisterSpells('MAGE', 80000, 1, {
 	[ 32612] =     66, -- Invisibility
 	[ 41425] =  45438, -- Hypothermia <- Ice Block
 	[ 44544] = 112965, -- Fingers of Frost (Frost)
+	[ 82691] = 113724, -- Ring of Frost (Frost talent)
+	[116014] = 116011, -- Rune of Power (Frost talent)
 	[190446] = 190447, -- Brain Freeze (Frost)
 	[205708] = { -- Chilled (Frost)
 		   116, -- Frostbolt
@@ -105,11 +119,26 @@ lib:__RegisterSpells('MAGE', 80000, 1, {
 		190356, -- Blizzard
 	},
 	[205473] =  76613, -- Icicles <- Mastery: Icicles (Frost)
+	[205766] = 205027, -- Bone Chilling (Frost talent)
 	[212792] =    120, -- Cone of Cold (Frost)
+	[228600] = 199786, -- Glacial Spike (Frost talent)
 	[228354] =  44614, -- Flurry (Frost)
+	[270232] = 270233, -- Freezing Rain (Frost talent)
+	[278310] = 278309, -- Chain Reaction (Frost talent)
 }, {
 	-- map aura to modified spell(s)
 	[ 44544] =  30455, -- Fingers of Frost (Frost) -> Ice Lance
 	[190446] =  44614, -- Brain Freeze (Frost) -> Flurry
-	[205473] =  30455, -- Icicles (Frost) -> Ice Lance
+	[205473] =  { -- Icicles (Frost)
+		 30455, -- Ice Lance
+		199786, -- Glacial Spike (Frost talent)
+	},
+	[205766] = { -- Bone Chilling (Frost talent)
+		   116, -- Frostbolt
+		 84714, -- Frozen Orb
+		190356, -- Blizzard
+		205021, -- Ray of Frost (Frost talent)
+	},
+	[270232] = 190356, -- Freezing Rain (Frost talent) -> Blizzard
+	[278310] =  30455, -- Chain Reaction (Frost talent) -> Ice Lance
 })
