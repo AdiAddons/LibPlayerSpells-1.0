b4bbe24cb76138deafe3b91f444d5d644a8cad32
Rainrider <Rainrider@users.noreply.github.com>
2018-07-03 16:06:19 +0200

    shaman: add elemental honor talents

--- data/Shaman.lua
+++ data/Shaman.lua
@@ -32,11 +32,13 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 				118297, -- Immolate (Fire Elemental) (Elemental talent)
 				157375, -- Eye of the Storm (Storm Elemental) (Elemental talent)
 				188389, -- Flame Shock (Elemental)
+				208997, -- Counterstrike Totem (Elemental honor talent)
 				CROWD_CTRL = {
 					[ 51514] = 'INCAPACITATE', -- Hex
 					STUN = {
 						118345, -- Pulverize (Earth Elemental) (Elemental talent)
 						118905, -- Static Charge
+						204437, -- Lightning Lasso (Elemental honor talent)
 					},
 				},
 				SNARE = {
@@ -45,7 +47,9 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 				},
 			},
 			HELPFUL = {
+				  8178, -- Grounding Totem Effect (Elemental honor talent)
 				192082, -- Wind Rush (Elemental talent)
+				208963, -- Skyfury Totem (Elemental honor talent)
 			},
 			PERSONAL = {
 				 77762, -- Lava Surge (Elemental)
@@ -54,6 +58,7 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 				173183, -- Elemental Blast: Haste (Elemental talent)
 				173184, -- Elemental Blast: Mastery (Elemental talent)
 				210714, -- Icefury (Elemental talent)
+				236746, -- Control of Lava (Elemental honor talent)
 				260734, -- Master of the Elements (Elemental talent)
 				263806, -- Wind Gust (Elemental talent)
 				BURST = {
@@ -74,6 +79,9 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 		HARMFUL = {
 			269808, -- Exposed Elements (Elemental talent)
 			[182387] = 'KNOCKBACK', -- Earthquake (Elemental)
+			CROWD_CTRL = {
+				[204399] = 'STUN', -- Earthfury (Elemental honor talent)
+			},
 			SNARE = {
 				196840, -- Frost Shock
 			},
@@ -83,8 +91,9 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 			974, -- Earth Shield
 		},
 		PERSONAL = {
-			2645, -- Ghost Wolf
-			6196, -- Far Sight
+			  2645, -- Ghost Wolf
+			  6196, -- Far Sight
+			204262, -- Spectral Recovery (Elemental honor talent)
 			SURVIVAL = {
 				260881, -- Spirit Wolf (Elemental talent)
 			},
@@ -97,6 +106,7 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 }, {
 	-- map aura to provider(s)
 	[  3600] =   2484, -- Earthbind <- Earthbind Totem
+	[  8178] = 204336, -- Grounding Totem Effect (Elemental honor talent) <- Grounding Totem
 	[ 77762] =  77756, -- Lava Surge (Elemental)
 	[118297] = 117013, -- Immolate <- Primal Elementalist (Elemental talent)
 	[118337] = 117013, -- Harden Skin <- Primal Elementalist (Elemental talent)
@@ -109,6 +119,11 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 	[173184] = 117014, -- Elemental Blast: Mastery (Elemental totem) <- Elemental Blast (Elemental talent)
 	[182387] =  61882, -- Earthquake (Elemental)
 	[192082] = 192077, -- Wind Rush <- Wind Rush Totem (Elemental talent)
+	[204262] = 204261, -- Spectral Recovery (Elemental honor talent)
+	[204399] = 204398, -- Earthfury (Elemental honor talent)
+	[208963] = 204330, -- Skyfury Totem (Elemental honor talent)
+	[208997] = 204331, -- Counterstrike Totem (Elemental honor talent)
+	[236746] = 204393, -- Control of Lava (Elemental honor talent)
 	[260734] =  16166, -- Master of the Elements (Elemental talent)
 	[260881] = 260878, -- Spirit Wolf (Elemental talent)
 	[263806] = 192249, -- Wind Gust <- Storm Elemental (Elemental talent)
@@ -125,7 +140,10 @@ lib:__RegisterSpells('SHAMAN', 80000, 1, {
 		188196, -- Lighting Bolt
 		188443, -- Chain Lightning
 	},
+	[204262] =   2645, -- Spectral Recovery (Elemental honor talent) -> Ghost Wolf
+	[204399] =   8042, -- Earthfury (Elemental honor talent) -> Earth Shock
 	[210714] = 196840, -- Icefury (Elemental talent) -> Frost Shock
+	[236746] =  51505, -- Control of Lava (Elemental honor talent) -> Lava Burst
 	[260734] =  51505, -- Master of the Elements (Elemental talent) -> Lava Burst
 	[260881] =   2645, -- Spirit Wolf (Elemental talent)
 	[263806] = { -- Wind Gust (Elemental talent)
