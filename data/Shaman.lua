c57223fc7b2d9c6946680aa0037e3ceec86036c5
Rainrider <Rainrider@users.noreply.github.com>
2018-07-10 20:16:16 +0200

    Battle for Azeroth Shaman (#131)

--- data/Shaman.lua
+++ data/Shaman.lua
@@ -15,223 +15,285 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
+along with LibPlayerSpells-1.0. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-local lib = LibStub("LibPlayerSpells-1.0")
+local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
-lib:__RegisterSpells("SHAMAN", 70300, 1, {
+lib:__RegisterSpells('SHAMAN', 80000, 1, {
 	COOLDOWN = {
-		    556, -- Astral Recall
-		  17364, -- Stormstrike
-		  51533, -- Feral Spirit
-		 115356, -- Windstrike
-		 157375, -- Gale Force (from Primal Stone Elemental) (knockback)
-		 192063, -- Gust of Wind
-		 192222, -- Liquid Magma Totem
-		 192249, -- Stone Elemental
-		 198067, -- Fire Elemental
-		 196884, -- Feral Lunge
-		 197995, -- Wellspring
+		   8143, -- Tremor Totem
+		  17364, -- Stormstrike (Enhancement)
+		  51505, -- Lava Burst (Elemental)
+		  51533, -- Feral Spirit (Enhancement)
+		 115356, -- Windstrike (Enhancement talent) NOTE: when Ascendance is active
+		 196884, -- Feral Lunge (Enhancement talent)
+		 197995, -- Wellspring (Restoration talent)
+		 198067, -- Fire Elemental (Elemental)
 		 198103, -- Earth Elemental
-		[ 57994] = "INTERRUPT", -- Wind Shear
+		 207778, -- Downpour (Restoration talent)
+		[ 57994] = 'INTERRUPT', -- Wind Shear
+		[193786] = 'POWER_REGEN', -- Rockbiter (Enhancement)
 		AURA = {
-			HELPFUL = {
-				 61295, -- Riptide
-				 98007, -- Spirit Link Totem
-				157384, -- Eye of the Storm (Primal Stone Elemental)
-				192082, -- Wind Rush
-				201633, -- Earthen Shield
-				207778, -- Gift of the Queen (Restoration artifact)
-				208899, -- Queen's Decree (Restoration artifact)
-			},
 			HARMFUL = {
-				118297, -- Immolate (Primal Fire Elemental)
-				188089, -- Earthen Spike
+				118297, -- Immolate (Fire Elemental) (Elemental talent)
+				157375, -- Eye of the Storm (Storm Elemental) (Elemental talent)
+				188089, -- Earthen Spike (Enhancement talent)
+				188389, -- Flame Shock (Elemental)
 				188838, -- Flame Shock (Restoration)
-				240842, -- Lashing Flames (Enhancement artifact)
+				208997, -- Counterstrike Totem (honor talent)
+				210927, -- Static Cling (Enhancement honor talent)
+				268429, -- Searing Assault (Enhancement talent)
+				271924, -- Molten Weapon (Enhancement talent)
 				CROWD_CTRL = {
+					[64695] = 'ROOT', -- Earthgrab (Restoration talent)
 					INCAPACITATE = {
-						 51514, -- Hex (incapacitate)
-						196942, -- Hex (Voodoo Totem) (incapacitate)
-					},
-					ROOT = {
-						 64695, -- Earthgrab (root)
-						197214, -- Sundering (root)
+						 51514, -- Hex
+						197214, -- Sundering (Enhancement talent)
 					},
 					STUN = {
-						118345, -- Pulverize (Primal Earth Elemental) (stun)
-						118905, -- Static Charge (stun)
+						118345, -- Pulverize (Earth Elemental) (Elemental talent)
+						118905, -- Static Charge
+						204437, -- Lightning Lasso (Elemental honor talent)
 					},
 				},
 				SNARE = {
-					 51490, -- Thunderstorm (slow) (knockback)
-					116947, -- Earthbind (slow)
-					224126, -- Frozen Bite (Enhancement artifact) (slow)
+					   3600, -- Earthbind
+					 116947, -- Earthbind (Restoration talent)
+					[ 51490] = 'KNOCKBACK', -- Thunderstorm (Elemental)
+				},
+			},
+			HELPFUL = {
+				  8178, -- Grounding Totem Effect (honor talent)
+				 61295, -- Riptide (Restoration)
+				192082, -- Wind Rush (talent)
+				201633, -- Earthen Wall (Restoraton talent)
+				204366, -- Thundercharge (Enhancement honor talent)
+				BURST = {
+					204361, -- Bloodlust (Enhancement honor talent)
+					208963, -- Skyfury Totem (honor talent)
+				},
+				SURVIVAL = {
+					 98007, -- Spirit Link Totem (Restoration)
+					207498, -- Ancestral Protection (Restoration talent)
 				},
 			},
 			PERSONAL = {
-				 58875, -- Spirit Walk
-				 73685, -- Unleash Life
-				 73920, -- Healing Rain
-				 79206, -- Spiritwalker's Grace
-				108281, -- Ancestral Guidance
-				114052, -- Ascendance (Restoration)
-				118522, -- Elemental Blast: Critical Strike
-				157504, -- Cloudburst Totem
-				173183, -- Elemental Blast: Haste
-				173184, -- Elemental Blast: Mastery
-				187878, -- Crash Lightning
-				191634, -- Static Overload (Elemental artifact)
-				191877, -- Power of Maelstrom (Elemental artifact)
-				198249, -- Elemental Healing (Enhancement artifact)
-				198300, -- Gathering Storms (Enhancement artifact)
-				202004, -- Landslide
-				204945, -- Doom Winds (Enhancement artifact)
-				205495, -- Stormkeeper (Elemental artifact)
-				210714, -- Icefury
-				215785, -- Hot Hand
-				215864, -- Rainfall
+				 58875, -- Spirit Walk (Enhancement)
+				 73685, -- Unleash Life (Restoration talent)
+				 73920, -- Healing Rain (Restoration)
+				 77762, -- Lava Surge (Elemental/Restoration)
+				 79206, -- Spiritwalker's Grace (Restoration)
+				108281, -- Ancestral Guidance (Elemental talent)
+				118522, -- Elemental Blast: Crtical Strike (Elemental talent)
+				157504, -- Cloudburst Totem (Restoration talent)
+				173183, -- Elemental Blast: Haste (Elemental talent)
+				173184, -- Elemental Blast: Mastery (Elemental talent)
+				187878, -- Crash Lightning (Enhancement)
+				194084, -- Flametongue (Enhancement)
+				202004, -- Landslide (Enhancement talent)
+				210714, -- Icefury (Elemental talent)
+				211400, -- Static Cling (Enhancement honor talent)
+				224125, -- Molten Weapon (Enhancement talent)
+				224127, -- Crackling Surge (Enhancement talent)
+				236502, -- Tidebringer (Restoration honor talent)
+				236746, -- Control of Lava (Elemental honor talent)
+				260734, -- Master of the Elements (Elemental talent)
+				263806, -- Wind Gust (Elemental talent)
+				BURST = {
+					114050, -- Ascendance (Elemental talent)
+					114051, -- Ascendance (Enhancement talent)
+					114052, -- Ascendance (Restoration talent)
+					191634, -- Stormkeeper (Elemental talent)
+				},
 				SURVIVAL = {
 					108271, -- Astral Shift
-					209950, -- Caress of the Tidemother (Restoration artifact)
-				},
-				BURST = {
-					 16166, -- Elemental Mastery
-					114050, -- Ascendance (Elemental)
-					114051, -- Ascendance (Enhancement)
-					201898, -- Windsong
+					118337, -- Harden Skin (Earth Elemental) (Elemental talent)
+					210918, -- Ethereal Form (Enhancement honor talent)
 				},
 			},
 			PET = {
-				118337, -- Harden Skin (Primal Earth Elemental)
-				157348. -- Call Lightning (Primal Stone Elemental)
+				157348, -- Call Lightning (Storm Elemental) (Elemental talent)
 			},
 		},
 	},
 	AURA = {
-		HELPFUL = {
-			   546, -- Water Walking
-			207400, -- Ancestral Vigor
-		},
 		HARMFUL = {
-			 182387, -- Earthquake
-			 188389, -- Flame Shock (Elemental)
-			 197209, -- Lightning Rod
-			[ 77505] = "CROWD_CTRL STUN", -- Earthquake (stun)
+			206647, -- Electrocute (Restoration honor talent)
+			269808, -- Exposed Elements (Elemental talent)
+			[182387] = 'KNOCKBACK', -- Earthquake (Elemental)
+			CROWD_CTRL = {
+				[204399] = 'STUN', -- Earthfury (Elemental honor talent)
+			},
 			SNARE = {
-				196840, -- Frost Shock (slow)
-				197385, -- Fury of Air (slow)
+				147732, -- Frostbrand (Enhancement)
+				196840, -- Frost Shock (Elemental)
+				197385, -- Fury of Air (Enhancement talent)
 			},
 		},
+		HELPFUL = {
+			   546, -- Water Walking
+			   974, -- Earth Shield (talent)
+			204293, -- Spirit Link (Restoration honor talent)
+			207400, -- Ancestral Vigor (Restoration talent)
+		},
 		PERSONAL = {
-			   2645, -- Ghost Wolf
-			   6196, -- Far Sight
-			  53390, -- Tidal Waves
-			  77762, -- Lava Surge
-			 192106, -- Lightning Shield
-			 194084, -- Flametongue
-			 196834, -- Frostbrand
-			 201846, -- Stormbringer
-			 202192, -- Resonance Totem
-			 210652, -- Storm Totem
-			 210658, -- Ember Totem
-			 210659, -- Tailwind Totem
-			[207527] = "SURVIVAL", -- Ghost in the Mist (Restoration artifact)
+			  2645, -- Ghost Wolf
+			  6196, -- Far Sight
+			 53390, -- Tidal Waves (Restoration)
+			192106, -- Lightning Shield (Enhancement talent)
+			196834, -- Frostbrand (Enhancement)
+			197211, -- Fury of Air (Enhancement talent)
+			201846, -- Stormbringer (Enhancement)
+			202192, -- Resonance Totem (Elemental talent)
+			204262, -- Spectral Recovery (honor talent)
+			210652, -- Storm Totem (Elemental talent)
+			210658, -- Ember Totem (Elemental talent)
+			210659, -- Tailwind Totem (Elemental talent)
+			215785, -- Hot Hand (Enhacement talent)
+			216251, -- Undulation (Restoration talent)
+			262397, -- Storm Totem (Enhancement talent)
+			262399, -- Ember Totem (Enhancement talent)
+			262400, -- Tailwind Totem (Enhancement talent)
+			262417, -- Resonance Totem (Enhancement talent)
+			262652, -- Forceful Winds (Enhancement talent)
+			273323, -- Lightning Shield Overcharge (Enhancement talent)
+			280815, -- Flash Flood (Restoration talent)
+			SURVIVAL = {
+				260881, -- Spirit Wolf (talent)
+			},
 		},
 	},
 	DISPEL = {
-		[  370] = "HARMFUL MAGIC", -- Purge
-		[51886] = "HELPFUL COOLDOWN CURSE", -- Cleanse Spirit
-		[77130] = "HELPFUL COOLDOWN CURSE MAGIC", -- Purify Spirit
+		[370] = 'HARMFUL MAGIC', -- Purge
+		HELPFUL = {
+			[51886] = 'CURSE', -- Cleanse Spirit (Elemental/Enhancement)
+			[77130] = 'CURSE MAGIC', -- Purify Spirit (Restoration)
+		},
 	},
 }, {
 	-- map aura to provider(s)
-	[ 53390] = 51564, -- Tidal Waves
-	[ 64695] = 51485, -- Earthgrab (root) <- Earthgrab Totem
-	[ 77505] = 61882, -- Earthquake (stun) <- Earthquake Totem
-	[ 77762] = 77756, -- Lava Surge
-	[ 98007] = 98008, -- Spirit Link Totem
-	[116947] = 51485, -- Earthbind (slow) <- Earthgrab Totem
-	[118522] = 117014, -- Elemental Blast: Critical Strike <- Elemental Blast
-	[118905] = 192058, -- Static Charge (stun) <- Lightning Surge Totem
-	[157384] = 157382, -- Eye of the Storm (Primal Stone Elemental)
-	[157504] = 157153, -- Cloudburst Totem
-	[173183] = 117014, -- Elemental Blast: Haste <- Elemental Blast
-	[173184] = 117014, -- Elemental Blast: Mastery <- Elemental Blast
-	[182387] = 61882, -- Earthquake (knockback) <- Earthquake Totem
-	[187878] = 187874, -- Crash Lightning
-	[191634] = 191602, -- Static Overload (Elemental artifact)
-	[191877] = 191861, -- Power of Maelstrom (Elemental artifact)
-	[192082] = 192077, -- Wind Rush
-	[194084] = 193796, -- Flametounge
-	[196942] = 196932, -- Hex (incapacitate) <- Voodoo Totem
-	[197209] = 210689, -- Lightning Rod
-	[197385] = 197211, -- Fury of Air (slow)
-	[198249] = 198248, -- Elemental Healing (Enhancement artifact)
-	[198300] = 198299, -- Gathering Storms (Enhancement artifact)
-	[201633] = 198838, -- Earthen Shield <- Earthen Shield Totem
-	[201846] = 201845, -- Stormbringer
-	[202004] = 197992, -- Landslide
-	[202192] = 210643, -- Resonance Totem <- Totem Mastery
-	[207400] = 207401, -- Ancestral Vigor
-	[207527] = 207351, -- Ghost in the Mist (Restoration artifact)
-	[208899] = 207360, -- Queen's Decree (Restoration artifact)
-	[209950] = 207354, -- Caress of the Tidemother (Restoration artifact)
-	[210652] = 210643, -- Storm Totem <- Totem Mastery
-	[210658] = 210643, -- Ember Totem <- Totem Mastery
-	[210659] = 210643, -- Tailwind Totem <- Totem Mastery
-	[215785] = 201900, -- Hot Hand
-	[224126] = 198505, -- Frozen Bite (Enhancement artifact) (slow) <- Doom Wolves
-	[240842] = 238142, -- Lashing Flames (Enhancement artifact)
+	[  3600] =   2484, -- Earthbind <- Earthbind Totem
+	[  8178] = 204336, -- Grounding Totem Effect (honor talent) <- Grounding Totem
+	[ 53390] =  51564, -- Tidal Waves (Restoration)
+	[ 64695] =  51485, -- Earthgrab <- Earthgrab Totem (Restoration talent)
+	[ 77762] =  77756, -- Lava Surge (Elemental/Restoration)
+	[ 98007] =  98008, -- Spirit Link Totem (Restoration)
+	[116947] =  51485, -- Earthbind <- Earthgrab Totem (Restoration talent)
+	[118297] = 117013, -- Immolate <- Primal Elementalist (Elemental talent)
+	[118337] = 117013, -- Harden Skin <- Primal Elementalist (Elemental talent)
+	[118345] = 117013, -- Pulverize <- Primal Elementalist (Elemental talent)
+	[118522] = 117014, -- Elemental Blast: Crtical Strike (Elemental totem) <- Elemental Blast (Elemental talent)
+	[118905] = 192058, -- Static Charge <- Capacitor Totem
+	[147732] = 196834, -- Frostbrand (Enhancement)
+	[157348] = 117013, -- Call Lightning <- Primal Elementalist (Elemental talent)
+	[157375] = 117013, -- Eye of the Storm <- Primal Elementalist (Elemental talent)
+	[157504] = 157153, -- Cloudburst Totem (Restoration talent)
+	[173183] = 117014, -- Elemental Blast: Haste (Elemental totem) <- Elemental Blast (Elemental talent)
+	[173184] = 117014, -- Elemental Blast: Mastery (Elemental totem) <- Elemental Blast (Elemental talent)
+	[182387] =  61882, -- Earthquake (Elemental)
+	[187878] = 187874, -- Crash Lightning (Enhancement)
+	[192082] = 192077, -- Wind Rush <- Wind Rush Totem (talent)
+	[194084] = 193796, -- Flametongue (Enhancement)
+	[197385] = 197211, -- Fury of Air (Enhancement talent)
+	[201633] = 198838, -- Earthen Wall <- Earthen Wall Totem (Restoraton talent)
+	[201846] = 201845, -- Stormbringer (Enhancement)
+	[202004] = 197992, -- Landslide (Enhancement talent)
+	[202192] = 210643, -- Resonance Totem <- Totem Mastery (Elemental talent)
+	[204262] = 204261, -- Spectral Recovery (honor talent)
+	[204361] = 193876, -- Bloodlust <- Shamanism (Enhancement honor talent)
+	[204399] = 204398, -- Earthfury (Elemental honor talent)
+	[206647] = 206642, -- Electrocute (Restoration honor talent)
+	[207400] = 207401, -- Ancestral Vigor (Restoration talent)
+	[207498] = 207399, -- Ancestral Protection <- Ancestral Protection Totem (Restoration talent)
+	[208963] = 204330, -- Skyfury Totem (honor talent)
+	[208997] = 204331, -- Counterstrike Totem (honor talent)
+	[210652] = 210643, -- Storm Totem <- Totem Mastery (Elemental talent)
+	[210658] = 210643, -- Ember Totem <- Totem Mastery (Elemental talent)
+	[210659] = 210643, -- Tailwind Totem <- Totem Mastery (Elemental talent)
+	[210927] = 211062, -- Static Cling (Enhancement honor talent)
+	[211400] = 211062, -- Static Cling (Enhancement honor talent)
+	[215785] = 201900, -- Hot Hand (Enhacement talent)
+	[216251] = 200071, -- Undulation (Restoration talent)
+	[224125] = 262624, -- Molten Weapon <- Elemental Spirits (Enhancement talent)
+	[224127] = 262624, -- Crackling Surge <- Elemental Spirits (Enhancement talent)
+	[236502] = 236501, -- Tidebringer (Restoration honor talent)
+	[236746] = 204393, -- Control of Lava (Elemental honor talent)
+	[260734] =  16166, -- Master of the Elements (Elemental talent)
+	[260881] = 260878, -- Spirit Wolf (talent)
+	[262397] = 262395, -- Storm Totem <- Totem Mastery (Enhancement talent)
+	[262399] = 262395, -- Ember Totem <- Totem Mastery (Enhancement talent)
+	[262400] = 262395, -- Tailwind Totem <- Totem Mastery (Enhancement talent)
+	[262417] = 262395, -- Resonance Totem <- Totem Mastery (Enhancement talent)
+	[262652] = 262647, -- Forceful Winds (Enhancement talent)
+	[263806] = 192249, -- Wind Gust <- Storm Elemental (Elemental talent)
+	[268429] = 192087, -- Searing Assault (Enhancement talent)
+	[269808] = 260694, -- Exposed Elements (Elemental talent)
+	[271924] = 262624, -- Molten Weapon <- Elemental Spirits (Enhancement talent)
+	[273323] = 192106, -- Lightning Shield Overcharge <- Lightning Shield (Enhancement talent)
+	[280815] = 280614, -- Flash Flood (Restoration talent)
 }, {
 	-- map aura to modified spell(s)
-	[ 53390] = { -- Tidal Waves
+	[ 53390] =  { -- Tidal Waves (Restoration)
 		 8004, -- Healing Surge
 		77472, -- Healing Wave
 	},
-	[ 77762] = 51505, -- Lava Surge -> Lava Burst
-	[114050] = { -- Ascendance
-		 51505, -- Lava Burst
-		114074, -- Lava Beam
-	},
-	[157504] = { -- Cloudburst Totem
-		201764, -- Recall Cloudburst Totem
-	},
-	[191634] = 188443, -- Static Overload (Elemental artifact) -> Chain Lightning
-	[191877] = 188196, -- Power of Maelstrom (Elemental artifact) -> Lightning Bolt
-	[197209] = { -- Lightning Rod
-		188196, -- Lightning Bolt
+	[ 77762] =  51505, -- Lava Surge (Elemental/Restoration) -> Lava Burst
+	[118297] = 118297, -- Immolate (Fire Elemental) (Elemental talent)
+	[118337] = 118337, -- Harden Skin (Earth Elemental) (Elemental talent)
+	[118345] = 118345, -- Pulverize (Earth Elemental) (Elemental talent)
+	[157348] = 157348, -- Call Lightning (Storm Elemental) (Elemental talent)
+	[157375] = 157375, -- Eye of the Storm (Storm Elemental) (Elemental talent)
+	[157504] = 201764, -- Cloudburst Totem (Restoration talent) -> Recall Cloudburst Totem
+	[191634] = { -- Stormkeeper (Elemental talent)
+		188196, -- Lighting Bolt
 		188443, -- Chain Lightning
 	},
-	[198249] = 108271, -- Elemental Healing (Enhancement artifact) -> Astral Shift
-	[198300] = { -- Gathering Storms (Enhancement artifact)
+	[201846] =  {-- Landslide (Enhancement talent)
 		 17364, -- Stormstrike
-		115356, -- Windstrike
+		115356, -- Windstrike (Enhancement talent) NOTE: when Ascendance is active
 	},
-	[201846] = { -- Stormbringer
+	[202004] =  { -- Landslide (Enhancement talent)
 		 17364, -- Stormstrike
-		115356, -- Windstrike
+		115356, -- Windstrike (Enhancement talent) NOTE: when Ascendance is active
 	},
-	[202004] = 193786, -- Landslide -> Rockbiter
-	[205495] = { -- Stormkeeper (Elemental artifact)
-		188196, -- Lightning Bolt
-		188443, -- Chain Lightning
-	},
-	[207400] = { -- Ancestral Vigor
+	[204262] =   2645, -- Spectral Recovery (honor talent) -> Ghost Wolf
+	[204361] = 204361, -- Bloodlust (Enhancement honor talent)
+	[204399] =   8042, -- Earthfury (Elemental honor talent) -> Earth Shock
+	[206647] =    370, -- Electrocute (Restoration honor talent) -> Purge
+	[207400] = { -- Ancestral Vigor (Restoration talent)
 		 1064, -- Chain Heal
 		 8004, -- Healing Surge
-		61295, -- Riptide
+		73920, -- Riptide
 		77472, -- Healing Wave
 	},
-	[207527] = 2645, -- Ghost in the Mist (Restoration artifact) -> Ghost Wolf
-	[208899] = 5394, -- Queen's Decree (Restoration artifact) -> Healing Stream Totem
-	[209950] = 5394, -- Caress of the Tidemother (Restoration artifact) -> Healing Strem Totem
-	[210714] = 196840, -- Icefury -> Frost Shock
-	[215785] = 60103, -- Hot Hand -> Lava Lash
-	[224126] = 51533, -- Frozen Bite (Enhancement artifact) (slow) -> Feral Spirit
-	[240842] = { -- Lashing Flames (Enhancement artifact)
-		 60103, -- Lava Lash (modified)
-		193796, -- Flametongue (provider)
+	[210714] = 196840, -- Icefury (Elemental talent) -> Frost Shock
+	[210927] =  17364, -- Static Cling (Enhancement honor talent) -> Stormstrike NOTE: not with Windstrike
+	[211400] =  17364, -- Static Cling (Enhancement honor talent) -> Stormstrike NOTE: not with Windstrike
+	[215785] =  60103, -- Hot Hand (Enhacement talent) -> Lava Lash
+	[216251] = { -- Undulation (Restoration talent)
+		 8004, -- Healing Surge
+		77472, -- Healing Wave
+	},
+	[224125] =  60103, -- Molten Weapon (Enhancement talent) -> Lava Lash
+	[224127] =  17364, -- Crackling Surge (Enhancement talent) -> Stormstrike
+	[236502] =   1064, -- Tidebringer (Restoration honor talent) -> Chain Heal
+	[236746] =  51505, -- Control of Lava (Elemental honor talent) -> Lava Burst
+	[260734] =  51505, -- Master of the Elements (Elemental talent) -> Lava Burst
+	[260881] =   2645, -- Spirit Wolf (talent)
+	[262652] =   6603, -- Forceful Winds (Enhancement talent) -> Auto Attack NOTE: Windfury
+	[263806] = { -- Wind Gust (Elemental talent)
+		188196, -- Lighting Bolt
+		188443, -- Chain Lightning
+	},
+	[268429] = 193796, -- Searing Assault (Enhancement talent) -> Flametongue
+	[269808] = 188196, -- Exposed Elements (Elemental talent) -> Lightning Bolt
+	[271924] =  60103, -- Molten Weapon (Enhancement talent) -> Lava Lash
+	[280815] = { -- Flash Flood (Restoration talent)
+		  1064, -- Chain Heal
+		  8004, -- Healing Surge
+		 73920, -- Healing Rain
+		 77472, -- Healing Wave
+		197995, -- Wellspring
 	},
 })
