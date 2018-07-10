66c605296013f57e60bd527c1f0495d40d0b7748
Rainrider <Rainrider@users.noreply.github.com>
2018-07-10 21:21:13 +0200

    rogue: add outlaw baseline abilities

--- data/Rogue.lua
+++ data/Rogue.lua
@@ -21,8 +21,81 @@ along with LibPlayerSpells-1.0. If not, see <http://www.gnu.org/licenses/>.
 local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
 lib:__RegisterSpells('ROGUE', 80000, 1, {
+	COOLDOWN = {
+		   1725, -- Distract
+		 195457, -- Grappling Hook (Outlaw)
+		[  1766] = 'INTERRUPT', -- Kick
+		AURA = {
+			HARMFUL = {
+				CROWD_CTRL = {
+					DISORIENT = {
+						2094, -- Blind
+					},
+					INCAPACITATE = {
+						1766, -- Gouge (Outlaw)
+					},
+					STUN = {
+						199804, -- Between the Eyes (Outlaw)
+					},
+				},
+			},
+			HELPFUL = {
+				57934, -- Tricks of the Trade
+			},
+			PERSONAL = {
+				  2987, -- Sprint
+				 11327, -- Vanish
+				 13877, -- Blade Flurry (Outlaw)
+				114018, -- Shroud of Concealment
+				BURST = {
+					13750, -- Adrenalin Rush (Outlaw)
+				},
+				SURVIVAL = {
+					  1966, -- Feint
+					 31224, -- Cloak of Shadows
+					185311, -- Crimson Vial
+					199754, -- Riposte (Outlaw)
+				},
+			},
+		},
+	},
+	AURA = {
+		HARMFUL = {
+			CROWD_CTRL = {
+				INCAPACITATE = {
+					6770, -- Sap
+				},
+				STUN = {
+					1833, -- Cheap Shot
+				},
+			},
+			SNARE = {
+				185763, -- Pistol Shot (Outlaw)
+			},
+		},
+		HELPFUL = {},
+		PERSONAL = {
+			  1784, -- Stealth
+			193356, -- Broadside (Outlaw)
+			193357, -- Ruthless Precision (Outlaw)
+			193358, -- Grand Melee (Outlaw)
+			193359, -- True Bearing (Outlaw)
+			195627, -- Opportunity (Outlaw)
+			199600, -- Buried Treasure (Outlaw)
+			199603, -- Skull and Crossbones (Outlaw)
+		},
+	},
 }, {
 	-- map aura to provider(s)
+	[ 11327] =   1856, -- Vanish
+	[193356] = 193316, -- Broadside <- Roll the Bones (Outlaw)
+	[193357] = 193316, -- Ruthless Precision <- Roll the Bones (Outlaw)
+	[193358] = 193316, -- Grand Melee <- Roll the Bones (Outlaw)
+	[193359] = 193316, -- True Bearing <- Roll the Bones (Outlaw)
+	[195627] = 193315, -- Opportunity <- Sinister Strike (Outlaw)
+	[199600] = 193316, -- Buried Treasure <- Roll the Bones (Outlaw)
+	[199603] = 193316, -- Skull and Crossbones <- Roll the Bones (Outlaw)
 }, {
 	-- map aura to modified spell(s)
+	[195627] = 185763, -- Opportunity -> Pistol Shot (Outlaw)
 })
