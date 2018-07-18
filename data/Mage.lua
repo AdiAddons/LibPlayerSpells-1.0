eb24cdb03ca24c179d74e957ea908810a90caf40
Rainrider <Rainrider@users.noreply.github.com>
2018-07-19 01:33:42 +0200

    mage: update for Battle for Azeroth (#130)
    Missing:
    - arcane
    - fire mage honor talents

--- data/Mage.lua
+++ data/Mage.lua
@@ -15,203 +15,185 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with LibPlayerSpells-1.0.  If not, see <http://www.gnu.org/licenses/>.
+along with LibPlayerSpells-1.0. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-local lib = LibStub("LibPlayerSpells-1.0")
+local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
-lib:__RegisterSpells("MAGE", 70300, 2, {
+lib:__RegisterSpells('MAGE', 80000, 1, {
 	COOLDOWN = {
 		   1953, -- Blink
-		  31687, -- Summon Water Elemental
-		  44425, -- Arcane Barrage
-		  55342, -- Mirror Image
-		  84714, -- Frozen Orb
-		 153595, -- Comet Storm
-		 153626, -- Arcane Orb
-		 157980, -- Supernova (knockback)
-		 190356, -- Blizzard
-		 194466, -- Phoenix's Flames (Fire artifact)
-		 198929, -- Cinderstorm
-		 205032, -- Charged Up
-		 212653, -- Shimmer
-		 214634, -- Ebonbolt (Frost artifact)
-		[  2139] = "INTERRUPT", -- Counterspell
-		[ 30449] = "DISPEL HARMFUL MAGIC", -- Spellsteal
-		[235219] = "SURVIVAL", -- Cold Snap
+		  31687, -- Summon Water Elemental (Frost)
+		  55342, -- Mirror Image (talent)
+		  84714, -- Frozen Orb (Frost)
+		 153595, -- Comet Storm (Frost talent)
+		 190336, -- Conjure Refreshment
+		 190356, -- Blizzard (Frost)
+		 212653, -- Shimmer (talent)
+		 257537, -- Ebonbolt (Frost talent)
+		[   475] = 'DISPEL HELPFUL CURSE', -- Remove Curse
+		[  2139] = 'INTERRUPT', -- Counterspell
+		[235219] = 'SURVIVAL', -- Cold Snap (Frost)
 		AURA = {
-			HELPFUL = {
-				240671, -- Warmth of the Phoenix (Fire artifact)
-			},
 			HARMFUL = {
-				135029, -- Water Jet (Water Elemental)
-				155158, -- Meteor
-				157997, -- Ice Nova (root) -- TODO: check category
-				194522, -- Blast Furnace (Fire artifact)
-				217694, -- Living Bomb
-				224968, -- Mark of Aluneth (Arcane artifact)
+				155158, -- Meteor Burn (Fire talent)
+				217694, -- Living Bomb (Fire talent) NOTE: initial
+				244813, -- Living Bomb (Fire talent) NOTE: spread
 				CROWD_CTRL = {
-					[31661] = "DISORIENT", -- Dragon's Breath (disorient)
-					[82691] = "INCAPACITATE", -- Ring of Frost (incapacitate)
+					[31661] = 'DISORIENT', -- Dragon's Breath (Fire)
+					[82691] = 'INCAPACITATE', -- Ring of Frost (talent)
 					ROOT = {
-						  122, -- Frost Nova (root)
-						33395, -- Freeze (Water Elemental) (root)
+						   122, -- Frost Nova
+						 33395, -- Freeze (Frost - Water Elemental)
+						157997, -- Ice Nova (Frost talent)
 					},
 				},
 				SNARE = {
-					157981, -- Blast Wave (slow)
-					205021, -- Ray of Frost (slow)
-					212792, -- Cone of Cold (slow)
-					236299, -- Chrono Shift (slow)
+					 205021, -- Ray of Frost (Frost talent)
+					 212792, -- Cone of Cold (Frost)
+					[157981] = 'KNOCKBACK', -- Blast Wave (Fire talent)
 				},
 			},
 			PERSONAL = {
-				 108839, -- Ice Floes
-				 113862, -- Greater Invisibility (dmg reduction)
-				 116014, -- Rune of Power
-				 194316, -- Cauterizing Blink (Fire artifact)
-				 198111, -- Temporal Shield (PvP)
-				 205025, -- Presence of Mind
-				 208166, -- Ray of Frost
-				 210126, -- Arcane Familiar
-				 212799, -- Displacement
-				 227482, -- Scorched Earth (fire artifact)
-				 236298, -- Chrono Shift
-				 240555, -- Freezing Rain (Frost artifact)
-				[ 12051] = "POWER_REGEN", -- Evocation
-				[ 41425] = "INVERT_AURA", -- Hypothermia
+				  11426, -- Ice Barrier (Frost)
+				 108839, -- Ice Floes (Frost talent)
+				 198111, -- Temporal Shield (Frost honor talent)
+				 198144, -- Ice Form (Frost honor talent)
+				 206432, -- Burst of Cold (Frost honor talent)
+				 270232, -- Freezing Rain (Frost talent)
+				[ 41425] = 'INVERT_AURA', -- Hypothermia
+				BURST = {
+					 12472, -- Icy Veins (Frost)
+					116014, -- Rune of Power (talent)
+					190319, -- Combustion (Fire)
+				},
 				SURVIVAL = {
 					    66, -- Invisibility (Fading)
-					 11426, -- Ice Barrier
 					 32612, -- Invisibility
 					 45438, -- Ice Block
-					110960, -- Greater Invisibility
-					235313, -- Blazing Barrier
-					235450, -- Prismatic Barrier
+					198065, -- Prismatic Cloak (honor talent)
+					235313, -- Blazing Barrier (Fire)
 				},
-				BURST = {
-					 12042, -- Arcane Power
-					 12472, -- Icy Veins
-					190319, -- Combustion
-					195446, -- Chilled to the Core (Frost artifact)
-				}
 			},
 		},
 	},
 	AURA = {
-		HELPFUL = {
-			130, -- Slow Fall
-		},
 		HARMFUL = {
-			112948, -- Frost Bomb
-			114923, -- Nether Tempest
-			210824, -- Touch of the Magi (Arcane artifact)
-			226757, -- Conflagration
-			228358, -- Winter's Chill
+			 12654, -- Ignite (Fire)
+			226757, -- Conflagrate (Fire talent)
+			228358, -- Winter's Chill (Frost)
 			CROWD_CTRL = {
-				[228600] = "ROOT", -- Glacial Spike (root) -- TODO: check category
+				[228600] = 'ROOT', -- Glacial Spike (Frost talent)
 				INCAPACITATE = {
-					   118, -- Polymorph (incapacitate)
-					 28271, -- Polymorph: Turtle (incapacitate)
-					 28272, -- Polymorph: Pig (incapacitate)
-					 61305, -- Polymorph: Black Cat (incapacitate)
-					 61721, -- Polymorph: Rabbit (incapacitate)
-					 61780, -- Polymorph: Turkey (incapacitate)
-					126819, -- Polymorph: Pig (porcupine) (incapacitate)
-					161353, -- Polymorph: Polar Bear Cub (incapacitate)
-					161354, -- Polymorph: Monkey (incapacitate)
-					161355, -- Polymorph: Penguin (incapacitate)
-					161372, -- Polymorph: Monkey (incapacitate)
+					   118, -- Polymorph (Sheep)
+					 28271, -- Polymorph (Turtle)
+					 28272, -- Polymorph (Pig)
+					 61305, -- Polymorph (Black Cat)
+					 61721, -- Polymorph (Rabbit)
+					 61780, -- Polymorph (Turkey)
+					126819, -- Polymorph (Porcupine)
+					161353, -- Polymorph (Polar Bear Cub)
+					161354, -- Polymorph (Monkey)
+					161355, -- Polymorph (Penguin)
+					161372, -- Polymorph (Peacock)
+					277787, -- Polymorph (Direhorn)
+					277792, -- Polymorph (Bumblebee)
 				},
 			},
 			SNARE = {
-				  2120, -- Flamestrike (slow)
-				 31589, -- Slow (slow)
-				205708, -- Chilled (slow)
-				228354, -- Flurry (slow)
+				  2120, -- Flamestrike (Fire)
+				205708, -- Chilled (Frost)
+				228354, -- Flurry (Frost)
 			},
 		},
-		PERSONAL = {
-			 44544, -- Fingers of Frost
-			 48108, -- Hot Streak!
-			 79683, -- Arcane Missiles!
-			157644, -- Enhanced Pyrotechnics
-			190446, -- Brain Freeze
-			195391, -- Jouster (Frost artifact)
-			195418, -- Chain Reaction (Frost artifact)
-			199844, -- Glacial Spike!
-			205473, -- Icicles
-			236060, -- Frenetic Speed
+		HELPFUL = {
+			  130, -- Slow Fall
+			[1459] = 'RAIDBUFF', -- Arcane Intellect
 		},
+		PERSONAL = {
+			 44544, -- Fingers of Frost (Frost)
+			 48108, -- Hot Streak! (Fire)
+			157644, -- Enhanced Pyrotechnics (Fire)
+			190446, -- Brain Freeze (Frost)
+			205473, -- Icicles (Frost)
+			205766, -- Bone Chilling (Frost talent)
+			236060, -- Frenetic Speed (Fire talent)
+			269651, -- Pyroclasm (Fire talent)
+			278310, -- Chain Reaction (Frost talent)
+		},
+	},
+	DISPEL = {
+		[30449] = 'HARMFUL MAGIC', -- Spellsteal
 	},
 }, {
 	-- map aura to provider(s)
-	[ 32612] = 66, -- Invisibility
-	[ 41425] = 45438, -- Hypothermia <- Ice Block
-	[ 44544] = 112965, -- Fingers of Frost
-	[ 48108] = 195283, -- Hot Streak! <- Hot Streak
-	[ 79683] = 5143, -- Arcane Missiles! -> Arcane Missiles
-	[ 82691] = 113724, -- Ring of Frost
-	[110960] = 110959, -- Greater Invisibility
-	[113862] = 110959, -- Greater Invisibility (dmg reduction)
-	[116014] = 116011, -- Rune of Power
-	[155158] = 153561, -- Meteor
-	[157644] = 157642, -- Enhanced Pyrotechnics
-	[190446] = 190447, -- Brain Freeze
-	[194316] = 194318, -- Cauterizing Blink (Fire artifact)
-	[194522] = 194487, -- Blast Furnace (Fire artifact)
-	[195391] = 214626, -- Jouster (Frost artifact)
-	[195418] = 195419, -- Chain Reaction (Frost artifact)
-	[195446] = 195448, -- Chilled to the Core (Frost artifact)
-	[199844] = 199786, -- Glacial Spike! <- Glacial Spike
-	[205473] = 76613, -- Icicles <- Mastery: Icicles
-	[205708] = { -- Chilled (slow)
+	[ 12654] =  12846, -- Ignite (Fire) <- Mastery: Ignite
+	[ 32612] =     66, -- Invisibility
+	[ 41425] =  45438, -- Hypothermia <- Ice Block
+	[ 44544] = 112965, -- Fingers of Frost (Frost)
+	[ 48108] = 195283, -- Hot Streak! (Fire)
+	[ 82691] = 113724, -- Ring of Frost (talent)
+	[116014] = 116011, -- Rune of Power (talent)
+	[155158] = 153561, -- Meteor Burn (Fire talent)
+	[157644] = 157642, -- Enhanced Pyrotechnics (Fire)
+	[190446] = 190447, -- Brain Freeze (Frost)
+	[198065] = 198064, -- Prismatic Cloak (honor talent)
+	[205708] = { -- Chilled (Frost)
 		   116, -- Frostbolt
 		 84714, -- Frozen Orb
 		190356, -- Blizzard
 	},
-	[208166] = 205021, -- Ray of Frost
-	[210126] = 205022, -- Arcane Familiar
-	[210824] = 210725, -- Touch of the Magi
-	[212792] = 120, -- Cone of Cold (slow)
-	[212799] = 195676, -- Displacement
-	[217694] = 44457, -- Living Bomb
-	[226757] = 205023, -- Conflagration
-	[227482] = 227481, -- Scorched Earth (Fire artifact)
-	[228354] = 44614, -- Flurry (slow)
-	[228358] = 190447, -- Winter's Chill <- Brain Freeze
-	[228600] = 199786, -- Glacial Spike (root)
-	[236060] = 236058, -- Frenetic Speed
-	[236298] = 235711, -- Chrono Shift
-	[236299] = 235711, -- Chrono Shift (slow)
-	[240555] = 238092, -- Freezing Rain (Frost artifact)
-	[240671] = 238091, -- Warmth of the Phoenix (Fire artifact)
+	[205473] =  76613, -- Icicles <- Mastery: Icicles (Frost)
+	[205766] = 205027, -- Bone Chilling (Frost talent)
+	[206432] = 206431, -- Burst of Cold (Frost honor talent)
+	[212792] =    120, -- Cone of Cold (Frost)
+	[217694] =  44457, -- Living Bomb (Fire talent) NOTE: initial
+	[226757] = 205023, -- Conflagrate (Fire talent)
+	[228358] = 231584, -- Winter's Chill (Frost) <- Brain Freeze (Rank 2)
+	[228600] = 199786, -- Glacial Spike (Frost talent)
+	[228354] =  44614, -- Flurry (Frost)
+	[236060] = 236058, -- Frenetic Speed (Fire talent)
+	[244813] =  44457, -- Living Bomb (Fire talent) NOTE: spread
+	[269651] = 269650, -- Pyroclasm (Fire talent)
+	[270232] = 270233, -- Freezing Rain (Frost talent)
+	[278310] = 278309, -- Chain Reaction (Frost talent)
 }, {
 	-- map aura to modified spell(s)
-	[ 44544] = 30455, -- Fingers of Frost -> Ice Lance
-	[ 48108] = { -- Hot Streak!
+	[ 12654] = { -- Ignite (Fire)
+		   133, -- Fireball
+		  2120, -- Flamestrike
+		  2948, -- Scorch
+		 11366, -- Pyroblast
+		108853, -- Fire Blast
+		153561, -- Meteor (Fire talent)
+		257541, -- Phoenix Flames (Fire talent)
+	},
+	[ 44544] =  30455, -- Fingers of Frost (Frost) -> Ice Lance
+	[ 48108] = { -- Hot Streak! (Fire)
 		 2120, -- Flamestrike
 		11366, -- Pyroblast
 	},
-	[157644] = 133, -- Enhanced Pyrotechnics -> Fireball
-	[190446] = 44614, -- Brain Freeze -> Flurry
-	[194316] = { -- Cauterizing Blink (Fire artifact)
+	[157644] =    133, -- Enhanced Pyrotechnics (Fire) -> Fireball
+	[190446] =  44614, -- Brain Freeze (Frost) -> Flurry
+	[198065] = { -- Prismatic Cloak (honor talent)
 		  1953, -- Blink
-		212653, -- Shimmer
+		212653, -- Shimmer (talent)
 	},
-	[194522] = 108853, -- Blast Furnace (Fire artifact) -> Fire Blast
-	[195391] = 30455, -- Jouster (Frost artifact) -> Ice Lance
-	[195418] = 30455, -- Chain Reaction (Frost artifact) -> Ice Lance
-	[195446] = 12472, -- Chilled to the Core (Frost artifact) -> Icy Veins
-	[205025] = 30451, -- Presence of Mind -> Arcane Blast
-	[205473] = 116, -- Icicles -> Frostbolt
-	[210824] = 30451, -- Touch of the Magi (Arcane artifact)
-	[226757] = 133, -- Conflagration -> Fireball
-	[227482] = 2948, -- Scorched Earth (Fire artifact) -> Scorch
-	[228358] = 44614, -- Winter's Chill -> Flurry
-	[236060] = 2948, -- Frenetic Speed -> Scorch
-	[236298] = 44425, -- Chrono Shift -> Arcane Barrage
-	[236299] = 44425, -- Chrono Shift (slow) -> Arcane Barrage
-	[240555] = 190356, -- Freezing Rain (Frost artifact) -> Blizzard
-	[240671] = 194466, -- Warmth of the Phoenix (Fire artifact) -> Phoenix's Flames (Fire artifact)
+	[205473] =  { -- Icicles (Frost)
+		 30455, -- Ice Lance
+		199786, -- Glacial Spike (Frost talent)
+	},
+	[205766] = { -- Bone Chilling (Frost talent)
+		   116, -- Frostbolt
+		 84714, -- Frozen Orb
+		190356, -- Blizzard
+		205021, -- Ray of Frost (Frost talent)
+	},
+	[206432] =    120, -- Burst of Cold (Frost honor talent) -> Cone of Cold
+	[226757] =    133, -- Conflagrate (Fire talent) -> Fireball
+	[228358] =  44614, -- Winter's Chill (Frost) -> Flurry
+	[236060] =   2948, -- Frenetic Speed (Fire talent) -> Scorch
+	[269651] =  11366, -- Pyroclasm (Fire talent) -> Pyroblast
+	[270232] = 190356, -- Freezing Rain (Frost talent) -> Blizzard
+	[278310] =  30455, -- Chain Reaction (Frost talent) -> Ice Lance
 })
