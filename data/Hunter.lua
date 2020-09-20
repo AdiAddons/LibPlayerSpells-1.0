b6b4fffc56b08b1afdb08b7bcad35187cff18b54
Rainrider <Rainrider@users.noreply.github.com>
2020-09-20 10:14:42 +0200

    hunter: bump spell revision number

--- data/Hunter.lua
+++ data/Hunter.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0. If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
-lib:__RegisterSpells('HUNTER', 80100, 2, {
+lib:__RegisterSpells('HUNTER', 80100, 3, {
 	COOLDOWN = {
 		    781, -- Disengage
 		  19434, -- Aimed Shot (Marksmanship)
