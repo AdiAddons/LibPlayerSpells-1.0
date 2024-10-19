0366b513013e9c4275f3dcdca03c6c4788255488
Rainrider <Rainrider@users.noreply.github.com>
2024-10-19 22:41:52 +0200

    test: drop luaunit in favor of busted

---
+++ tests/databases_spec.lua
@@ -0,0 +1,37 @@
+package.path = package.path .. ';./wowmock/?.lua'
+
+local wowmock = require('wowmock')
+
+describe('Imports', function ()
+	local G, lib
+
+	before_each(function ()
+		G = mock({ C_Spell = {
+			GetSpellInfo = function (id) return { spellID = id } end}
+		})
+		lib = wowmock('../LibPlayerSpells-1.0.lua', G)
+	end)
+
+	for _, source in next, {
+		'Deathknight',
+		'Demonhunter',
+		'Druid',
+		'Evoker',
+		'Hunter',
+		'Mage',
+		'Monk',
+		'Paladin',
+		'Priest',
+		'Racials',
+		'Rogue',
+		'Shaman',
+		'Warlock',
+		'Warrior',
+	} do
+		it(source, function ()
+			G.LibStub = function () return lib end
+
+			wowmock('../data/' .. source .. '.lua', G)
+		end)
+	end
+end)
